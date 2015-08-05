abedb43ffdc8ff3097f6a2879629ea95df7aca95
Chris Hanson <chris.f.hanson@gmail.com>
2015-08-05 22:51:58 +0930

    chore(*): setup gulp build system

---
+++ gulp/config.js
@@ -0,0 +1,29 @@
+var publicAssets = "./public/assets";
+var sourceFiles  = "./gulp/assets";
+
+module.exports = {
+  publicAssets: publicAssets,
+  browserSync: {
+    proxy: 'localhost:3000',
+    files: ['./app/views/**']
+  },
+  sass: {
+    src: sourceFiles + "/stylesheets/**/*.{sass,scss}",
+    dest: publicAssets + "/stylesheets",
+    settings: {
+      imagePath: '/assets/images' // Used by the image-url helper
+    }
+  },
+  images: {
+    src: sourceFiles + "/images/**",
+    dest: publicAssets + "/images"
+  },
+  browserify: {
+    bundleConfigs: [{
+      entries: sourceFiles + '/javascripts/app.js',
+      dest: publicAssets + '/javascripts',
+      outputName: 'app.js',
+      extensions: ['.js']
+    }]
+  }
+};

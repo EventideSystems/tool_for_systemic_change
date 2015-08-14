11f5ae7d5e7ba037e9343af7b97b9609e061fa7f
Tom Tuddenham <ferrisoxide@gmail.com>
2015-08-14 13:11:52 +0930

    Fixes #19, adds FFaker gem for use in factories

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,4 +1,5 @@
 class Organisation < ActiveRecord::Base
   belongs_to :adminstrating_organisation, class_name: 'AdministratingOrganisation'
+  belongs_to :sector
   has_and_belongs_to_many :intiatives, join_table: :initiatives_organisations, class_name: 'Initiative'
 end

219c257b0eed33b5d50d0bbef905be798cfbbea9
Tom Tuddenham <ferrisoxide@gmail.com>
2015-08-15 15:38:59 +0930

    Fixed #32 Add default users, etc to seeds

--- app/models/model/focus_area.rb
+++ app/models/model/focus_area.rb
@@ -1,4 +1,4 @@
 class Model::FocusArea < ActiveRecord::Base
   belongs_to :focus_area_group, class_name: 'Model::FocusAreaGroup'
-  has_many  :initiative_characteristics, class_name: 'Model::InitiatveCharacteristic'
+  has_many  :initiative_characteristics, class_name: 'Model::InitiativeCharacteristic'
 end

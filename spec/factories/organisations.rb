01c84e6e4f33ea0e592d2cdaf8ca37a810b2343f
Tom Tuddenham <ferrisoxide@gmail.com>
2015-08-22 17:05:56 +0930

    done - initiatives need to be refactored before more work can be done

--- spec/factories/organisations.rb
+++ spec/factories/organisations.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :organisation do
     name { FFaker::Company.name }
+    description { FFaker::Lorem.words.join(' ') }
 
     factory :administrating_organisation,  class: AdministratingOrganisation do
 

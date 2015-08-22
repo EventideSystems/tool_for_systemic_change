f09af50a8187c9abd96252b9db6e648ca651abac
Tom Tuddenham <ferrisoxide@gmail.com>
2015-08-23 00:21:21 +0930

    route specs now match to json format (default)

--- spec/routing/organisations_routing_spec.rb
+++ spec/routing/organisations_routing_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe OrganisationsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/organisations").to route_to("organisations#index")
+      expect(:get => "/organisations").to route_to("organisations#index", :format => 'json')
     end
 
     it "routes to #new" do
-      expect(:get => "/organisations/new").to route_to("organisations#new")
+      expect(:get => "/organisations/new").to route_to("organisations#new", :format => 'json')
     end
 
     it "routes to #show" do
-      expect(:get => "/organisations/1").to route_to("organisations#show", :id => "1")
+      expect(:get => "/organisations/1").to route_to("organisations#show", :id => "1", :format => 'json')
     end
 
     it "routes to #edit" do
-      expect(:get => "/organisations/1/edit").to route_to("organisations#edit", :id => "1")
+      expect(:get => "/organisations/1/edit").to route_to("organisations#edit", :id => "1", :format => 'json')
     end
 
     it "routes to #create" do
-      expect(:post => "/organisations").to route_to("organisations#create")
+      expect(:post => "/organisations").to route_to("organisations#create", :format => 'json')
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/organisations/1").to route_to("organisations#update", :id => "1")
+      expect(:put => "/organisations/1").to route_to("organisations#update", :id => "1", :format => 'json')
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/organisations/1").to route_to("organisations#update", :id => "1")
+      expect(:patch => "/organisations/1").to route_to("organisations#update", :id => "1", :format => 'json')
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/organisations/1").to route_to("organisations#destroy", :id => "1")
+      expect(:delete => "/organisations/1").to route_to("organisations#destroy", :id => "1", :format => 'json')
     end
 
   end

72722ab2fcb50fc97c42087c3c3cdde1ee97258f
Tom Tuddenham <ferrisoxide@gmail.com>
2015-08-31 20:58:16 +0930

    adds the ability to link organisations with initiatives

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -1,8 +1,11 @@
-# SMELL This whole class needs to be refactored. Doesn't make sense in the
-# context of a "checklist"
 class Initiative < ActiveRecord::Base
   belongs_to :wicked_problem
   has_and_belongs_to_many :organisations, join_table: :initiatives_organisations
+
+  # TODO Add a validation to ensure that organistions belong to same
+  # administrating organisation as the initiative. NB this might be an argument
+  # for converting the join table into a proper model and adding validations to
+  # it
 
   scope :for_user, ->(user) {
     unless user.staff?

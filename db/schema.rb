57fff473924c402686cf94f2a15c527ca8a7e92a
Tom Tuddenham <ferrisoxide@gmail.com>
2015-09-09 12:56:24 +0930

    basic VM working under Vagrant

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150907010921) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "communities", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
@@ -84,7 +87,7 @@ ActiveRecord::Schema.define(version: 20150907010921) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "sectors", ["name"], name: "index_sectors_on_name", unique: true
+  add_index "sectors", ["name"], name: "index_sectors_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                          default: "", null: false
@@ -112,11 +115,11 @@ ActiveRecord::Schema.define(version: 20150907010921) do
     t.integer  "invitations_count",              default: 0
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true
-  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count"
-  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
+  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
+  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "wicked_problems", force: :cascade do |t|
     t.string   "name"

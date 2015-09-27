1745a9785ae58be34e507ab9fbae798db3c8bcb7
Tom Tuddenham <ferrisoxide@gmail.com>
2015-09-27 20:47:20 +0930

    disable ssl verify for email

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    openssl_verify_mode:  'none',
     address:              'mail.eventidesystems.com',
     port:                 587,
     domain:               'eventidesystems.com',

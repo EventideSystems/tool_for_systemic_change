cf0fe4cd98c7344ba31afc223bd18e671b5972aa
Tom Tuddenham <ferrisoxide@gmail.com>
2015-09-27 13:56:58 +0930

    adds policy for invitations, status to user model

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < BaseSerializer
-  attributes :email, :role, :name, :created_at, :updated_at, :last_sign_in_at
+  attributes :email, :role, :name, :created_at, :updated_at, :last_sign_in_at, :status
 
   belongs_to :client
 end

f03ce0e689eb424ad020729801c268ed621d5d4e
Chris Hanson <chris.f.hanson@gmail.com>
2015-10-03 13:12:08 +0930

    feat(problems): adds problems/scorecard changes

---
+++ gulp/assets/javascripts/scorecards/initiative-controller.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+angular.module('WKD.Scorecards')
+
+.controller('WKD.Scorecards.InitiativeController', [
+  'flashr',
+  'Restangular',
+  'WKD.Common.DataModel',
+  'currentProblem', //injected by resolve
+  function (flashr, Restangular, dataModel, currentProblem) {
+    var vm = this;
+
+    vm.initiatives = _.where(currentProblem.included, { type: 'initiatives' });
+
+    vm.showChecklist = function (initiative) {
+      if (initiative.$showChecklist) {
+        initiative.$showChecklist = false;
+        return;
+      }
+
+      _.each(vm.initiatives, function (ini) { ini.$showChecklist = false; });
+
+      if (initiative.$focusGroups) {
+        initiative.$showChecklist = true;
+      } else {
+        initiative.$loading = true;
+
+        Restangular.one('initiatives', initiative.id)
+          .getList('checklist_items').then(function (resp) {
+            initiative.$loading = false;
+            initiative.$showChecklist = true;
+            initiative.$focusGroups = dataModel.dataModelFrom(resp.included);
+            dataModel.assignChecklistItems(initiative.$focusGroups, resp);
+          });
+      }
+    };
+
+    /////////////////////////////////////////////////////////////////////////
+  }
+]);

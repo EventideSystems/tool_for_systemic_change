585273619311d4d95ca472e72149e730f57091c7
Tom Tuddenham <ferrisoxide@gmail.com>
2015-10-05 09:15:42 +1030

    styled accept invitation ui

--- app/controllers/custom_invitations_controller.rb
+++ app/controllers/custom_invitations_controller.rb
@@ -1,4 +1,4 @@
-class InvitationsController < Devise::InvitationsController
+class CustomInvitationsController < Devise::InvitationsController
 
   before_filter :configure_permitted_parameters
   before_filter :set_default_role_param, only: :create
@@ -32,8 +32,28 @@ class InvitationsController < Devise::InvitationsController
 
   end
 
-  def update
 
+  def update
+    raw_invitation_token = update_resource_params[:invitation_token]
+    self.resource = accept_resource
+    invitation_accepted = resource.errors.empty?
+
+    yield resource if block_given?
+
+    if invitation_accepted
+      if Devise.allow_insecure_sign_in_after_accept
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message :notice, flash_message if is_flashing_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_accept_path_for(resource)
+      else
+        set_flash_message :notice, :updated_not_active if is_flashing_format?
+        respond_with resource, :location => new_session_path(resource_name)
+      end
+    else
+      resource.invitation_token = raw_invitation_token
+      respond_with_navigational(resource){ render :edit }
+    end
   end
 
 

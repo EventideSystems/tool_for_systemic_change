3428ddcbeaad398ffa65453ad5070337472a7088
Tom Tuddenham <ferrisoxide@gmail.com>
2015-10-12 08:40:21 +1030

    fixed #114

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   match 'initiatives/:initiative_id/checklist_items', to: 'checklist_items#bulk_update', via: [:put, :patch], :defaults => { :format => 'json' }
   resources :organisations, :defaults => { :format => 'json' }
+  resources :clients, :defaults => { :format => 'json' }
   resources :communities, :defaults => { :format => 'json' }
   resources :invitations, :controller => 'invitations', only: [:create], :defaults => { :format => 'json' }
   resources :scorecards, :defaults => { :format => 'json' }

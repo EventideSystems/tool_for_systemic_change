3a97628cedfbbd2a50233b1a4c8d6ae4a7bbcb3c
Tom Tuddenham <ferrisoxide@gmail.com>
2015-10-25 08:40:13 +1030

    fix missing fields in serializers

--- app/serializers/scorecard_initiative_serializer.rb
+++ app/serializers/scorecard_initiative_serializer.rb
@@ -1,5 +1,7 @@
 class ScorecardInitiativeSerializer < InitiativeSerializer
-  attributes :id, :name, :description, :created_at, :updated_at
+  attributes :id, :name, :description,
+             :started_at, :finished_at, :dates_confirmed,
+             :created_at, :updated_at
 
   has_many :checklist_items
 end

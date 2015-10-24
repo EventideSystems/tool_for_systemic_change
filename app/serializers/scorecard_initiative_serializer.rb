78e75bdb48c6cd1060d53fd0df4275d852df10d7
Tom Tuddenham <ferrisoxide@gmail.com>
2015-10-25 09:33:41 +1030

    adds contact details to initiative

--- app/serializers/scorecard_initiative_serializer.rb
+++ app/serializers/scorecard_initiative_serializer.rb
@@ -1,7 +1,12 @@
 class ScorecardInitiativeSerializer < InitiativeSerializer
   attributes :id, :name, :description,
              :started_at, :finished_at, :dates_confirmed,
-             :created_at, :updated_at
+             :created_at, :updated_at,
+             :contact_name,
+             :contact_email,
+             :contact_phone,
+             :contact_website,
+             :contact_position
 
   has_many :checklist_items
 end

56f7c4fae67c85e762685162e768a1e1058b5a95
Tom Tuddenham <ferrisoxide@gmail.com>
2015-11-01 10:58:01 +1030

    Adds endpoint for managing current client:
    GET /current_client # returns a client resource
    PUT /current_client (params :id => new_client_id) # updates current client
    
    NOTES
    * PUT /current_client is only available to `Staff` users
    * Current client id is stored in the session with the key `:staff_current_client_id`

---
+++ app/controllers/current_client_controller.rb
@@ -0,0 +1,23 @@
+class CurrentClientController < AuthenticatedController
+  before_action :authenticate_staff_user!, only: [:update]
+
+  def show
+    render json: current_client
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    session[:staff_current_client_id] = @client.id if @client
+
+    respond_to do |format|
+      if @client
+        format.json { render json: { status: :ok, location: current_client } }
+      else
+        format.json do
+          render json: "Unknown client id: #{params[:id]}",
+                 status: :unprocessable_entity
+        end
+      end
+    end
+  end
+end

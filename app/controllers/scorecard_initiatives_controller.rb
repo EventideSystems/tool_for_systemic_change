680a00b97b9e5967455a3128558c93c178fe6aec
Tom Tuddenham <ferrisoxide@gmail.com>
2016-01-17 21:07:50 +1030

    fixed issue with unauthenticated access to scorecard_initiatives

--- app/controllers/scorecard_initiatives_controller.rb
+++ app/controllers/scorecard_initiatives_controller.rb
@@ -1,6 +1,11 @@
 class ScorecardInitiativesController < AuthenticatedController
 
-  before_action :set_scorecard, only: [:show, :index]
+  before_action :set_scorecard, only: [:show]
+  before_action :set_scorecard_from_shared, only: [:index]
+
+  skip_before_filter :authenticate_user!, only: :index
+
+  class InvalidSharedLinkId < Exception; end
 
   resource_description do
     formats ['json']
@@ -34,6 +39,15 @@ class ScorecardInitiativesController < AuthenticatedController
       @scorecard = current_client.scorecards.find(params[:scorecard_id])
     rescue ActiveRecord::RecordNotFound
       raise User::NotAuthorized
+    end
+
+    def set_scorecard_from_shared
+      if /\A\d+\z/ =~ params[:scorecard_id] # param[:scorecard_id] is an integer
+        set_scorecard
+      else
+        @scorecard = Scorecard.find_by(shared_link_id: params[:scorecard_id])
+        raise InvalidSharedLinkId, 'Unknown shared link id' unless @scorecard
+      end
     end
 
 end

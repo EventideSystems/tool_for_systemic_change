081dcce58a8222d0941dfb7fe60f593072b7fd9f
Tom Tuddenham <ferrisoxide@gmail.com>
2016-02-09 15:09:24 +1030

    added resend invitation feature

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -60,6 +60,8 @@ class ApplicationPolicy
         user.staff?
       when :invite_user
         user.staff?
+      when :resend_invitation
+        user.staff?
       else
         raise ::Pundit::NotDefinedError, "unable to find policy #{action} for #{record}."
     end

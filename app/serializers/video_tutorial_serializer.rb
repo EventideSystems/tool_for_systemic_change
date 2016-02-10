db208e062314c498df7df0ee261fceaea35a247f
Tom Tuddenham <ferrisoxide@gmail.com>
2016-02-10 11:51:59 +1030

    additional fields / endpoints to support video tutorials

--- app/serializers/video_tutorial_serializer.rb
+++ app/serializers/video_tutorial_serializer.rb
@@ -1,5 +1,6 @@
 class VideoTutorialSerializer < BaseSerializer
-  attributes :id, :name, :description, :promote_to_dashboard, :link_url, :vimeo_id, :created_at, :updated_at
+  attributes :id, :name, :description, :promote_to_dashboard, :link_url,
+    :vimeo_id, :linked_type, :linked_id, :linked_name, :created_at, :updated_at
 
   belongs_to :linked
 end

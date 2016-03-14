2d4dfb44e65b2418a406e649587ea690a904d332
Tom Tuddenham <ferrisoxide@gmail.com>
2016-03-14 22:37:29 +1030

    client interface simplified, initia admin user created at creation of client

---
+++ lib/rails_admin/new_client.rb
@@ -0,0 +1,78 @@
+module RailsAdmin
+  module Config
+    module Actions
+      class NewClient < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+
+        register_instance_option :collection do
+          true
+        end
+
+        register_instance_option :http_methods do
+          [:get, :post] # NEW / CREATE
+        end
+
+        register_instance_option :controller do
+          proc do
+            if request.get? # NEW
+
+              @object = @abstract_model.new
+              @authorization_adapter && @authorization_adapter.attributes_for(:new, @abstract_model).each do |name, value|
+                @object.send("#{name}=", value)
+              end
+              if object_params = params[@abstract_model.to_param]
+                @object.set_attributes(@object.attributes.merge(object_params))
+              end
+              respond_to do |format|
+                format.html { render @action.template_name }
+                format.js   { render @action.template_name, layout: false }
+              end
+
+            elsif request.post? # CREATE
+
+              @modified_assoc = []
+              @object = @abstract_model.new
+              sanitize_params_for!(request.xhr? ? :modal : :create)
+
+              @object.set_attributes(params[@abstract_model.param_key])
+              @authorization_adapter && @authorization_adapter.attributes_for(:create, @abstract_model).each do |name, value|
+                @object.send("#{name}=", value)
+              end
+
+              if @object.save
+
+                # SMELL No handling of errors
+                if params[:client][:initial_admin_user_email]
+                  initial_admin_user = User.new(
+                    name: params[:client][:initial_admin_user_name],
+                    email: params[:client][:initial_admin_user_email],
+                    client_id: @object.id,
+                    role: 'admin'
+                  )
+                  initial_admin_user.invite!(current_user)
+                end
+
+                @auditing_adapter && @auditing_adapter.create_object(@object, @abstract_model, _current_user)
+                respond_to do |format|
+                  format.html { redirect_to_on_success }
+                  format.js   { render json: {id: @object.id.to_s, label: @model_config.with(object: @object).object_label} }
+                end
+              else
+                handle_save_error
+              end
+
+            end
+          end
+        end
+
+        register_instance_option :link_icon do
+          'icon-plus'
+        end
+
+        register_instance_option :template_name do
+          :new_client
+        end
+      end
+    end
+  end
+end

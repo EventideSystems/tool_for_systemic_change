18983bc08e3cea6ba89b7b68d561eb35e7848d5d
Tom Tuddenham <ferrisoxide@gmail.com>
2017-02-14 22:49:55 +1030

    fixtures in, tests failing... woot

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test 'authenticated users can GET index' do
+    sign_in users(:system_admin)
+
+    get '/'
+    assert_response :redirect
+  end
+
 end

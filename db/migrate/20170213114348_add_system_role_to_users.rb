6dce6eb93d568b0f9b3d826abcbb6eef43a0046a
Tom Tuddenham <ferrisoxide@gmail.com>
2017-02-15 22:39:36 +1030

    basic policy working on wicked problems

--- db/migrate/20170213114348_add_system_role_to_users.rb
+++ db/migrate/20170213114348_add_system_role_to_users.rb
@@ -1,6 +1,6 @@
 class AddSystemRoleToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :system_role, :string, default: 'regular' # Regular user
+    add_column :users, :system_role, :integer, default: 0
     add_index :users, :system_role
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -198,12 +198,12 @@ ActiveRecord::Schema.define(version: 20170213121243) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email",                  default: "",        null: false
-    t.string   "encrypted_password",     default: "",        null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,         null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -218,7 +218,7 @@ ActiveRecord::Schema.define(version: 20170213121243) do
     t.integer  "invitations_count",      default: 0
     t.datetime "deleted_at"
     t.string   "profile_picture"
-    t.string   "system_role",            default: "regular"
+    t.integer  "system_role",            default: 0
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
     t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree

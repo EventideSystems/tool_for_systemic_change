fa80efaaf1a71263427ed7060bae7f7445d7570d
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-11 00:56:30 +1030

    display flash if account not selected

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,12 +4,15 @@ class DashboardController < ApplicationController
   
   def index
     add_breadcrumb "Dashboard"
+    
+    flash[:alert] = "Select an account before continuing." unless current_account.present?
+    
     @scorecard_count = policy_scope(Scorecard).all.count
     @initiative_count = policy_scope(Initiative).all.count
     @complete_initiatives_count = policy_scope(Initiative).complete.count
     @incomplete_initiatives_count = policy_scope(Initiative).incomplete.count
     @overdue_initiatives_count = policy_scope(Initiative).overdue.count
-    @welcome_message = current_account.welcome_message
-    @recent_activities = policy_scope(PublicActivity::Activity).limit(10).order(created_at: :desc)
+    @welcome_message = current_account.present? ? current_account.welcome_message : ''
+    @recent_activities = current_account.present? ? policy_scope(PublicActivity::Activity).limit(10).order(created_at: :desc) : []
   end
 end

5bc7e7cc904c5b86d65221a91ecc8a2949752172
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-14 20:37:32 +1030

    updated summary boxes on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,9 +9,12 @@ class DashboardController < ApplicationController
     
     @scorecard_count = policy_scope(Scorecard).all.count
     @initiative_count = policy_scope(Initiative).all.count
-    @complete_initiatives_count = policy_scope(Initiative).complete.count
-    @incomplete_initiatives_count = policy_scope(Initiative).incomplete.count
-    @overdue_initiatives_count = policy_scope(Initiative).overdue.count
+    @wicked_problem_count = policy_scope(WickedProblem).all.count
+    @organisation_count = policy_scope(Organisation).all.count
+    
+    # @complete_initiatives_count = policy_scope(Initiative).complete.count
+    # @incomplete_initiatives_count = policy_scope(Initiative).incomplete.count
+    # @overdue_initiatives_count = policy_scope(Initiative).overdue.count
     @welcome_message = current_account.present? ? current_account.welcome_message : ''
     @recent_activities = current_account.present? ? policy_scope(PublicActivity::Activity).limit(10).order(created_at: :desc) : []
   end

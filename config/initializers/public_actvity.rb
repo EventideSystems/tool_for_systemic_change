5a38b9604913c107bedc470ab0569e7554311dc2
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-15 23:27:47 +1030

    another effort to solve circlci issue - redux - redux - redux

--- config/initializers/public_actvity.rb
+++ config/initializers/public_actvity.rb
@@ -1,6 +1,26 @@
+# frozen_string_literal: true
 require 'public_activity'
 
-Trackable.include PublicActivity::Model
+
+module Trackable
+  extend ActiveSupport::Concern
+
+  included do
+    
+    include PublicActivity::Model
+    
+    tracked owner: proc { |controller, _model|
+      controller ? controller.current_user : nil
+    }
+
+    tracked account_id: proc { |controller, _model|
+      if controller
+        controller.current_account ? controller.current_account.id : nil
+      end
+    }
+  end
+end
+
 
 [ Account,
   ChecklistItem,

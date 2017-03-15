3e07fe4b66d6dbc4bcbdd90a02f9048a14d01769
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-15 23:27:47 +1030

    another effort to solve circlci issue - redux - redux - redux

--- app/models/concerns/trackable.rb
+++ app/models/concerns/trackable.rb
@@ -1,17 +1,1 @@
-# frozen_string_literal: true
-module Trackable
-  extend ActiveSupport::Concern
 
-  included do
-    
-    tracked owner: proc { |controller, _model|
-      controller ? controller.current_user : nil
-    }
-
-    tracked account_id: proc { |controller, _model|
-      if controller
-        controller.current_account ? controller.current_account.id : nil
-      end
-    }
-  end
-end

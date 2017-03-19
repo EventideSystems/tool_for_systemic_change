02270191fe5729db9321ce5109d52b0be42c7331
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-19 11:04:09 +1030

    adding created_at to index pages

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,9 @@
 module ActivitiesHelper
+  def activity_summary(activity)
+    return '' unless activity.present?    
+    subject_type, action = *activity.key.split('.')
+    subject_name = activity.trackable.name
+    actor = activity.owner.is_a?(User) ? "by #{activity.owner.name}" : '' 
+    "#{subject_type.humanize.titleize} '#{subject_name}' #{action}d #{actor}"
+  end
 end

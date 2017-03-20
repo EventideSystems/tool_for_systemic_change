1cb2793d555affc45e7665deb3e27a725d57cbbc
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-20 21:27:41 +1030

    fixed video link model

--- app/models/video_tutorial.rb
+++ app/models/video_tutorial.rb
@@ -7,7 +7,8 @@ class VideoTutorial < ApplicationRecord
   scope :promoted_to_dashboard, -> {
     where(promote_to_dashboard: true).order(:position)
   }
-
+  
+  # TODO Validation based on video link format
   def linked_name
     return '' unless linked.present?
 
@@ -15,24 +16,20 @@ class VideoTutorial < ApplicationRecord
   end
   
   def thumbnail_iframe(height=90, width=90)
-    link_url.gsub(/height="(\d*)"/, "height=\"#{height}\"").gsub(/width="(\d*)"/, "width=\"#{width}\"").html_safe  
+    build_vimeo_iframe(vimeo_id_from_link(link_url), height, width)
   end
   
   def iframe(height=380, width=570)
-    link_url.gsub(/height="(\d*)"/, "height=\"#{height}\"").gsub(/width="(\d*)"/, "width=\"#{width}\"").html_safe  
-  end
-
-  def iframe_width
-    return nil unless link_url.present?
-    matches = link_url.match(/width="(\d.)"/)
-    return matches[1].to_i if matches && matches[1]
-    nil
+    build_vimeo_iframe(vimeo_id_from_link(link_url), height, width)
   end
   
-  def iframe_height
-    return nil unless link_url.present?
-    matches = link_url.match(/height="(\d.)"/)
-    return matches[1].to_i if matches && matches[1]
-    nil
+  private
+  
+  def vimeo_id_from_link(url)
+    url.match(/(\d*)\z/)[0]
+  end
+  
+  def build_vimeo_iframe(video_id, height, width)
+    "<iframe src=\"https://player.vimeo.com/video/#{video_id}\" width=\"#{width}\" height=\"#{height}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>".html_safe
   end
 end

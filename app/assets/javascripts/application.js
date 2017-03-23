b733d4448286e855fa4d61542c7c4674687e7a08
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-23 12:02:29 +1030

    rough preview for scorecards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,7 +39,73 @@
   });
   
   $(document).ready(function() {
-    $('.wizard').bootstrapWizard({'tabClass': 'nav nav-pills'});
+    $('.wizard').bootstrapWizard({
+      tabClass: 'nav nav-pills',
+      onTabShow: function(tab, navigation, index) {
+
+        if ($(tab).children().first().attr('href') == '#tab5') {
+          
+          function fetchWickedProblemDescription(index) {
+            $.ajax({
+              url: "/wicked_problems/" + index + ".json",
+              context: document.body,
+              success: function(data) {
+                $("#preview-wicked-problem-description").html(data["description"]); 
+              }
+            });
+          };
+          
+          function fetchCommunityDescription(index) {
+            $.ajax({
+              url: "/communities/" + index + ".json",
+              context: document.body,
+              success: function(data) {
+                $("#preview-community-description").html(data["description"]); 
+              }
+            });
+          };
+          
+          var scorecard_name = $('#scorecard_name').val();
+          var scorecard_desc = $('#scorecard_description').val();
+
+          if (scorecard_name === "") {
+            scorecard_name = '<em style="color:red">Missing</em>'
+          }
+          
+          $("#preview-scorecard-name").html(scorecard_name);
+          $("#preview-scorecard-description").html(scorecard_desc);
+          
+          var wicked_problem_name = $('#scorecard_wicked_problem_id option:selected').text();
+          var wicked_problem_id = $('#scorecard_wicked_problem_id').val();
+          
+          if (wicked_problem_name === "") {
+            wicked_problem_name = '<em style="color:red">Missing</em>'
+          }
+          
+          $("#preview-wicked-problem-name").html(wicked_problem_name);
+          fetchWickedProblemDescription(wicked_problem_id);
+          
+          var community_name = $('#scorecard_community_id option:selected').text();
+          var community_id = $('#scorecard_community_id').val();
+          
+          if (community_name === "") {
+            community_name = '<em style="color:red">Missing</em>'
+          }
+          
+          $("#preview-community-name").html(community_name);
+          fetchCommunityDescription(community_id);
+          
+          $('ul#preview-initiatives li').remove()
+          $('.initiative-name').each(function (index, value) { 
+            if ($(value).val() === "") {
+              $('ul#preview-initiatives').append('<li><em style="color:red">Name missing</em></li>');
+            } else {
+              $('ul#preview-initiatives').append('<li>'+$(value).val()+'</li>');
+            }
+          });
+        }
+      }
+    });
   });
   
   $(document).ready(function() {

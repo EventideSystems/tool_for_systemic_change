0799307f9bd304847f2a3dbdfd4b8877ab174d42
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-25 17:56:15 +1030

    fix bug in view initiative

--- app/helpers/initiatives_helper.rb
+++ app/helpers/initiatives_helper.rb
@@ -1,2 +1,21 @@
 module InitiativesHelper
+  
+  def scorecard_label(initiative)
+    if initiative.new_record? || initiative.scorecard.nil?
+      'Scorecard'
+    else  
+      safe_join([
+        content_tag(:span, 'Scorecard', style: 'margin-right: 10px'),
+        link_to(
+          content_tag(
+            :i, 
+            '', 
+            class: "fa fa-external-link"
+          ), 
+          scorecard_path(initiative.scorecard), 
+          id: 'initiative-detail-scorecard-link'
+        )
+      ])
+    end
+  end    
 end

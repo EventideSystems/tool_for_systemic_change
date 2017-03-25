54b9f4c2776d1b771aeea42ca47899d3184a481a
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-25 13:37:17 +1030

    #217 Vimeo - can't add private video

--- app/models/video_tutorial.rb
+++ app/models/video_tutorial.rb
@@ -26,10 +26,11 @@ class VideoTutorial < ApplicationRecord
   private
   
   def vimeo_id_from_link(url)
-    url.match(/(\d*)\z/)[0]
+    matches = url.match(/(\/\d+\z)|(\/\d+)[\/]?.+?\z/)
+    matches[matches.length-1] || matches[0]
   end
   
   def build_vimeo_iframe(video_id, height, width)
-    "<iframe src=\"https://player.vimeo.com/video/#{video_id}\" width=\"#{width}\" height=\"#{height}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>".html_safe
+    "<iframe src=\"https://player.vimeo.com/video#{video_id}\" width=\"#{width}\" height=\"#{height}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>".html_safe
   end
 end

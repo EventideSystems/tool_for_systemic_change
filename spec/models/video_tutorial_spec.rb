4e7c7f563623cda429da1b28be4a268200deb72c
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-25 13:37:17 +1030

    #217 Vimeo - can't add private video

--- spec/models/video_tutorial_spec.rb
+++ spec/models/video_tutorial_spec.rb
@@ -15,6 +15,17 @@ RSpec.describe VideoTutorial, type: :model do
       '<iframe src="https://player.vimeo.com/video/208056324" width="570" height="380" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
     )}
   end
+  
+  context 'private vimeo link' do
+    let(:link_url) { 'https://vimeo.com/208056324/607c549a71' }
+   
+    context 'thumbnail_iframe' do
+      it { expect(subject.thumbnail_iframe).to eq(
+        '<iframe src="https://player.vimeo.com/video/208056324" width="90" height="90" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+      )}
+    end
+  end
+  
 end
 
 

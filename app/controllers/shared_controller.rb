6588365f2b001d5c311d50f4385073eb4e555af3
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-26 12:12:40 +1030

    finished shared link

--- app/controllers/shared_controller.rb
+++ app/controllers/shared_controller.rb
@@ -1,4 +1,4 @@
-class SharedScorecardsController < ApplicationController
+class SharedController < ApplicationController
   skip_before_action :authenticate_user!
   skip_after_action :verify_policy_scoped
   skip_after_action :verify_authorized

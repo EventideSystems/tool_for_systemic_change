90d307fe0497734b75732e512d7cf49928d2a361
Tom Tuddenham <ferrisoxide@gmail.com>
2017-03-28 12:02:19 +1030

    first cut of pdf generator

--- spec/requests/accounts_request_spec.rb
+++ spec/requests/accounts_request_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "accounts page", :type => :request do
-  it "displays the user's username after successful login" do
+  it "creates a new account" do
     @user = FactoryGirl.create(:user)
     
     sign_in_as_a_valid_user
+    
+    post "/account", { account: blah blah... will have to write this eventually}
   end
 end

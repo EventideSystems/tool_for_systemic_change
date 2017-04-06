aeb541dad110973408f429aa09a9253d35709b0f
Tom Tuddenham <ferrisoxide@gmail.com>
2017-04-06 19:13:36 +0930

    fixes issue with problems creating new user with same email as old, added sector management pages

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -2,7 +2,7 @@
 class Sector < ApplicationRecord
   acts_as_paranoid
 
-  validates :name, presence: true, uniqueness: { scope: :account_id }
+  validates :name, presence: true
   
   has_many :accounts
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe VideoTutorial, type: :model do
+  
+  it 'allows a user to be recreated after a delete' do
+    user = User.create!(email: 'foo@bar.com', password: 'q2341234213', password_confirmation: 'q2341234213')
+    user.delete
+    
+    expect{ User.create!(email: 'foo@bar.com', password: 'q2341234213', password_confirmation: 'q2341234213')}
+      .to_not raise_exception
+  end
+end  

78b6db03684eb01399070894891b71848c7406f1
Tom Tuddenham <ferrisoxide@gmail.com>
2017-05-30 19:12:38 +0930

    Adds paper trail

--- app/models/initiatives_organisation.rb
+++ app/models/initiatives_organisation.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class InitiativesOrganisation < ApplicationRecord
+  has_paper_trail
   #validates :initiative, :organisation, presence: true
 
   belongs_to :initiative

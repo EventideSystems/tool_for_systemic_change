0ff4dd77a41a20b00f017bcfe17f0db7cc28d581
Tom Tuddenham <ferrisoxide@gmail.com>
2017-08-30 00:26:27 +0930

    fixed reports

--- app/models/reports/scorecard_activity.rb
+++ app/models/reports/scorecard_activity.rb
@@ -63,7 +63,7 @@ module Reports
         wrap_text = p.workbook.styles.add_style alignment: { horizontal: :general, vertical: :bottom, wrap_text: true }
         date = p.workbook.styles.add_style format_code: "d/m/yy"
         
-        p.workbook.add_worksheet do |sheet|
+        p.workbook.add_worksheet(name: 'Report') do |sheet|
           sheet.add_row(['Scorecard'], style: header_1).add_cell(scorecard.name, style: blue_normal)
           sheet.add_row(['Date range'], b: true).tap do |row|
             row.add_cell(date_from, style: date)

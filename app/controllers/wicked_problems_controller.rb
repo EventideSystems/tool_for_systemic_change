3941b286d6c99fdb2d0529767ea11bf47964a0f1
Tom Tuddenham <ferrisoxide@gmail.com>
2018-01-09 21:13:33 +1030

    fixes for form labels, etc

--- app/controllers/wicked_problems_controller.rb
+++ app/controllers/wicked_problems_controller.rb
@@ -4,7 +4,7 @@ class WickedProblemsController < ApplicationController
   before_action :set_wicked_problem, only: [:show, :edit, :update, :destroy]
   before_action :require_account_selected, only: [:new, :create, :edit, :update] 
 
-  add_breadcrumb "Wicked Problems", :wicked_problems_path
+  add_breadcrumb "Wicked Problems / Opportunities", :wicked_problems_path
   
   def index
     @wicked_problems = policy_scope(WickedProblem).order(sort_order).page params[:page]
@@ -18,7 +18,7 @@ class WickedProblemsController < ApplicationController
   def new
     @wicked_problem = current_account.wicked_problems.build
     authorize @wicked_problem
-    add_breadcrumb "New Wicked Problem"
+    add_breadcrumb "New"
   end
 
   def edit
@@ -31,7 +31,7 @@ class WickedProblemsController < ApplicationController
     
     respond_to do |format|
       if @wicked_problem.save
-        format.html { redirect_to wicked_problems_path, notice: 'Wicked problem was successfully created.' }
+        format.html { redirect_to wicked_problems_path, notice: 'Wicked problem / opportunity was successfully created.' }
         format.json { render :show, status: :created, location: @wicked_problem }
         format.js
       else
@@ -45,7 +45,7 @@ class WickedProblemsController < ApplicationController
   def update
     respond_to do |format|
       if @wicked_problem.update(wicked_problem_params)
-        format.html { redirect_to wicked_problems_path, notice: 'Wicked problem was successfully updated.' }
+        format.html { redirect_to wicked_problems_path, notice: 'Wicked problem / opportunity was successfully updated.' }
         format.json { render :show, status: :ok, location: @wicked_problem }
       else
         format.html { render :edit }
@@ -58,9 +58,13 @@ class WickedProblemsController < ApplicationController
     @wicked_problem.delete
     
     respond_to do |format|
-      format.html { redirect_to wicked_problems_url, notice: 'Wicked problem was successfully destroyed.' }
+      format.html { redirect_to wicked_problems_url, notice: 'Wicked problem / opportunity was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+  
+  def content_title
+    WickedProblem.model_name.human.pluralize
   end
   
   def content_subtitle

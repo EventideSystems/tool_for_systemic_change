f750f24a1884f30368a12e0dc61badefa28def7f
Tom Tuddenham <ferrisoxide@gmail.com>
2018-02-09 21:39:31 +1030

    fix 'scorecard' text

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -12,43 +12,43 @@ class SearchResultsController < ApplicationController
     policy_scope(WickedProblem)
       .where('name ILIKE ? OR description ILIKE ?', query, query)
       .each_with_object(@search_results) do |value, memo| 
-        memo << SearchResult.new(value.class, value.name, value.description, wicked_problem_path(value))
+        memo << SearchResult.new(value.model_name.human, value.name, value.description, wicked_problem_path(value))
       end
           
     policy_scope(Scorecard)
       .where('name ILIKE ? OR description ILIKE ?', query, query)
       .each_with_object(@search_results) do |value, memo| 
-        memo << SearchResult.new(value.class, value.name, value.description, scorecard_path(value))
+        memo << SearchResult.new(value.model_name.human, value.name, value.description, scorecard_path(value))
       end
       
     policy_scope(Initiative)
       .where('initiatives.name ILIKE ? OR initiatives.description ILIKE ?', query, query)
       .each_with_object(@search_results) do |value, memo| 
-        memo << SearchResult.new(value.class, value.name, value.description, initiative_path(value))
+        memo << SearchResult.new(value.model_name.human, value.name, value.description, initiative_path(value))
       end
       
     policy_scope(Community)
       .where('name ILIKE ? OR description ILIKE ?', query, query)
       .each_with_object(@search_results) do |value, memo| 
-        memo << SearchResult.new(value.class, value.name, value.description, community_path(value))
+        memo << SearchResult.new(value.model_name.human, value.name, value.description, community_path(value))
       end
       
     policy_scope(Organisation)
       .where('name ILIKE ? OR description ILIKE ?', query, query)
       .each_with_object(@search_results) do |value, memo| 
-        memo << SearchResult.new(value.class, value.name, value.description, organisation_path(value))
+        memo << SearchResult.new(value.model_name.human, value.name, value.description, organisation_path(value))
       end
       
     policy_scope(Account)
       .where('name ILIKE ? OR description ILIKE ?', query, query)
       .each_with_object(@search_results) do |value, memo| 
-        memo << SearchResult.new(value.class, value.name, value.description, account_path(value))
+        memo << SearchResult.new(value.model_name.human, value.name, value.description, account_path(value))
       end
       
     policy_scope(User)
       .where('name ILIKE ? OR email ILIKE ?', query, query)
       .each_with_object(@search_results) do |value, memo| 
-        memo << SearchResult.new(value.class, value.name, value.email, user_path(value))
+        memo << SearchResult.new(value.model_name.human, value.name, value.email, user_path(value))
       end
   end
 

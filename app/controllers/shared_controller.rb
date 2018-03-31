375eee4e8861b02122b0491b82980f1e3bf9ad51
Tom Tuddenham <ferrisoxide@gmail.com>
2018-04-01 07:56:53 +0930

    working

--- app/controllers/shared_controller.rb
+++ app/controllers/shared_controller.rb
@@ -3,9 +3,15 @@ class SharedController < ApplicationController
   skip_after_action :verify_policy_scoped
   skip_after_action :verify_authorized
   
+  layout 'embedded'
+  
   def show
     @focus_areas = FocusArea.ordered_by_group_position
     @scorecard = Scorecard.find_by_shared_link_id(params[:id])
-    render layout: 'embedded'
+    if params[:iframe] == 'true'
+      render 'show_iframe', layout: 'embedded'
+    else
+      render layout: 'embedded'
+    end
   end
 end

a1bec0f0997456e2c195d2f9b9ad261a8ddf4825
Tom Tuddenham <ferrisoxide@gmail.com>
2018-04-01 09:00:03 +0930

    finesse iframe settings

--- app/controllers/shared_controller.rb
+++ app/controllers/shared_controller.rb
@@ -3,14 +3,12 @@ class SharedController < ApplicationController
   skip_after_action :verify_policy_scoped
   skip_after_action :verify_authorized
   
-  layout 'embedded'
-  
   def show
     @focus_areas = FocusArea.ordered_by_group_position
     @scorecard = Scorecard.find_by_shared_link_id(params[:id])
     if params[:iframe] == 'true'
       response.headers.delete "X-Frame-Options"
-      render 'show_iframe', layout: 'embedded'
+      render 'show_iframe', layout: false
     else
       render layout: 'embedded'
     end

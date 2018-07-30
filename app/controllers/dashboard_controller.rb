2ecc022d64e6068342997fcbc37282cf721a2f21
Tom Tuddenham <ferrisoxide@gmail.com>
2018-07-30 17:48:32 +0930

    minor speed improvement to home controller dash

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,12 +7,10 @@ class DashboardController < ApplicationController
     
     flash[:alert] = "Select an account before continuing." unless current_account.present?
     
-    @scorecard_count = policy_scope(Scorecard).all.count
-    @initiative_count = policy_scope(Initiative).all.count
-    @wicked_problem_count = policy_scope(WickedProblem).all.count
-    @organisation_count = policy_scope(Organisation).all.count
-    
-    @video_tutorials = policy_scope(VideoTutorial).promoted_to_dashboard.all
+    @scorecard_count = policy_scope(Scorecard).count
+    @initiative_count = policy_scope(Initiative).count
+    @wicked_problem_count = policy_scope(WickedProblem).count
+    @organisation_count = policy_scope(Organisation).count
 
     @welcome_message = current_account.present? ? current_account.welcome_message : ''
     @recent_versions = current_account.present? ? policy_scope(PaperTrail::Version).limit(10).order(created_at: :desc) : []

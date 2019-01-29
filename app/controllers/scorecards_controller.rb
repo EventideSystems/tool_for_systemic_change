45a9372d646d966688eb7f7e0f3e62ecab05710c
Tom Tuddenham <ferrisoxide@gmail.com>
2019-01-29 22:32:20 +1030

    341 fix duplicate org or tag

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -199,26 +199,32 @@ class ScorecardsController < ApplicationController
       ).tap do |params| # NOTE Dupe of code in initiatives controller
         params[:initiatives_attributes].each do |initiative_key, _|
           params[:initiatives_attributes][initiative_key][:initiatives_organisations_attributes].reject! do |key, value|
-            value[:organisation_id].blank?
-          end
+            value[:_destroy] != '1' && (
+              value[:organisation_id].blank? || (
+                value[:id].blank? && 
+                params[:initiatives_attributes][initiative_key][:initiatives_organisations_attributes].to_h.any? do |selected_key, selected_value|
+                  selected_key != key &&
+                  selected_value[:_destroy] != '1' && 
+                  selected_value[:organisation_id] == value[:organisation_id]
+                end
+              )
+            )
+          end  
+        end
         
-          params[:initiatives_attributes][initiative_key][:initiatives_organisations_attributes].transform_values! do |value|
-            value[:_destroy] = '0' if value[:_destroy] == '1' &&
-              params[:initiatives_attributes][initiative_key][:initiatives_organisations_attributes].to_h.any? do |other_key, other_value|
-                other_value[:_destroy] != '1' && 
-                other_value[:organisation_id] == value[:organisation_id]
-              end
-
-            value
-          end
-        
-          params[:initiatives_attributes][initiative_key][:initiatives_organisations_attributes].reject! do |key, value|
-            (value[:id].nil?) &&  
-            params[:initiatives_attributes][initiative_key][:initiatives_organisations_attributes].to_h.any? do |other_key, other_value|
-              other_key != key && 
-              other_value[:organisation_id] == value[:organisation_id]
-            end
-          end
+        params[:initiatives_attributes].each do |initiative_key, _|
+          params[:initiatives_attributes][initiative_key][:initiatives_subsystem_tags_attributes].reject! do |key, value|
+            value[:_destroy] != '1' && (
+              value[:subsystem_tag_id].blank? || (
+                value[:id].blank? && 
+                params[:initiatives_attributes][initiative_key][:initiatives_subsystem_tags_attributes].to_h.any? do |selected_key, selected_value|
+                  selected_key != key &&
+                  selected_value[:_destroy] != '1' && 
+                  selected_value[:subsystem_tag_id] == value[:subsystem_tag_id]
+                end
+              )
+            )
+          end  
         end
       end
     end

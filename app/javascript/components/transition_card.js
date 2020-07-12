3a0c0d4dc96bf57961f9c82abad52528d92468ac
Tom Tuddenham <ferrisoxide@gmail.com>
2020-07-12 12:19:16 +0930

    refactor daterange picker

--- app/javascript/components/transition_card.js
+++ app/javascript/components/transition_card.js
@@ -4,3 +4,47 @@ $(document).on('turbolinks:load', function() {
   });
 });
 
+// Daterange controls
+
+$(document).on('turbolinks:load', function() {
+    var dateStart = $('#daterange-btn').data('selected-date');
+
+    if (dateStart === undefined) {
+      var parsedDateStart = moment()
+    } else {
+      var parsedDateStart = moment(dateStart, 'YYYY-MM-DD').format('MMMM D, YYYY')
+    }
+
+    $('#daterange-btn').daterangepicker(
+        {
+          locale: {
+            format: 'MMMM D, YYYY'
+          },
+          startDate: parsedDateStart,
+          opens: 'right',
+          singleDatePicker: true,
+        },
+        function (start, end) {
+          $('#daterange-btn span').html(start.format('MMMM D, YYYY'));
+        }
+    );
+    
+    $('#daterange-btn').on('apply.daterangepicker', function(ev, picker) {
+      var scorecardId = $(picker.element).data('scorecard-id');
+      var date = picker.startDate.format('YYYY-MM-DD');
+      var tags = $.map($('#subsystem-tags').select2('data'), function(v) { return v.text });
+      
+      var params = { selected_tags: tags, selected_date: date };
+      
+      var url = '/transition_cards/' + scorecardId + '?' + $.param(params);
+      $(location).attr('href', url)
+    });
+    
+    $('#daterange-clear-btn').click(function(e) {
+      var scorecardId = $('#daterange-btn').data('scorecardId')
+      var tags = $.map($('#subsystem-tags').select2('data'), function(v) { return v.text });
+      var params = { selected_tags: tags };
+      var url = '/transition_cards/' + scorecardId + '?' + $.param(params);
+      $(location).attr('href', url)   
+    });
+  });

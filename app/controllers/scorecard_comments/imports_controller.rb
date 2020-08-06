975ea40b79bad11ab7f886fdc3f140b2590fef76
Tom Tuddenham <ferrisoxide@gmail.com>
2020-08-06 22:32:43 +0930

    465 fix imports

--- app/controllers/scorecard_comments/imports_controller.rb
+++ app/controllers/scorecard_comments/imports_controller.rb
@@ -23,7 +23,7 @@ class ScorecardComments::ImportsController < ApplicationController
     
     @scorecard_comments_import.destroy
     file_system = Shrine.storages[:cache]
-    file_system.clear!(older_than: Time.now - 1.hour)
+    file_system.clear! { |path| path.mtime < Time.now - 1.hour } 
   end
 
   def update

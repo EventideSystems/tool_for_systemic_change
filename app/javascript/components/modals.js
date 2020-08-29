528a18127febf831ae4e6d5b19ced359ff5a5ee6
Tom Tuddenham <ferrisoxide@gmail.com>
2020-08-29 20:00:16 +0930

    460 first cut

--- app/javascript/components/modals.js
+++ app/javascript/components/modals.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function() {
   $('.remote-link').on('click', function(e) {
     var link = $(this).attr('href');
-    $('.modal').find(".modal-content").load(link);
-    $('.modal').modal('show');
-    e.preventDefault();
+    $('#default-modal').find(".modal-content").load(link);
+    $('#default-modal').modal('show');
+   // e.preventDefault();
   });
 });
 
@@ -19,5 +19,34 @@ $(document).on('turbolinks:load', function() {
     $('.modal').find(".modal-content").load(link);
     $('.modal').modal('show');
     e.preventDefault();
+  });
+});
+
+$(document).on('turbolinks:load', function() {
+
+  $('#ecosystem-maps-modal').on('shown.bs.modal', function () {
+
+    // $(document).off('focusin.bs.modal');
+
+    var ecosystemMapsTimer;
+
+    function hideEcosystemMapModal() {
+      $('#ecosystem-maps-modal').modal('hide')
+    };
+
+    console.log('starting timer');
+    ecosystemMapsTimer = setTimeout(hideEcosystemMapModal, 3000);
+
+    $('#ecosystem-maps-modal > .modal-dialog').on("mouseenter", function() {
+      console.log('mouseenter');
+      clearTimeout(ecosystemMapsTimer);
+       ecosystemMapsTimer = null;
+    });
+
+    $('#ecosystem-maps-modal > .modal-dialog').on("mouseleave", function() {
+      console.log('mouseleave');
+      clearTimeout(ecosystemMapsTimer);
+      ecosystemMapsTimer = setTimeout(hideEcosystemMapModal, 2000)
+    });
   });
 });

9567e65f38d8837b893eec7a0c6b481d140c928d
Tom Tuddenham <ferrisoxide@gmail.com>
2020-09-30 21:26:24 +0930

    OOB seeds for building database post crash

--- db/migrate/20170206100555_devise_invitable_add_to_users.rb
+++ db/migrate/20170206100555_devise_invitable_add_to_users.rb
@@ -1,4 +1,4 @@
-class DeviseInvitableAddToUsers < ActiveRecord::Migration
+class DeviseInvitableAddToUsers < ActiveRecord::Migration[5.0]
   def up
     change_table :users do |t|
       t.string     :invitation_token
@@ -17,7 +17,13 @@ class DeviseInvitableAddToUsers < ActiveRecord::Migration
   def down
     change_table :users do |t|
       t.remove_references :invited_by, polymorphic: true
-      t.remove :invitations_count, :invitation_limit, :invitation_sent_at, :invitation_accepted_at, :invitation_token, :invitation_created_at
+      t.remove \
+        :invitations_count, 
+        :invitation_limit, 
+        :invitation_sent_at, 
+        :invitation_accepted_at, 
+        :invitation_token, 
+        :invitation_created_at
     end
   end
 end

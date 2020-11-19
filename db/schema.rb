0c19d0c5615afca9d5c036d01259eab52cfd4483
Tom Tuddenham <ferrisoxide@gmail.com>
2020-11-19 22:50:45 +1030

    467 WIP, new TransitionCardSummary created and working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_111549) do
+ActiveRecord::Schema.define(version: 2020_11_19_111703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

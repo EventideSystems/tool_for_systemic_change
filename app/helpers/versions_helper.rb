0441afb83c65c1071c153f97d0ec630d7062e406
Tom Tuddenham <ferrisoxide@gmail.com>
2021-02-13 12:48:13 +1030

    OOB fix for unexpected classes in version table

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -2,7 +2,7 @@ module VersionsHelper
   def version_summary(version)
     return '' unless version.present?
     
-    item_name = version_item_name(version.item)
+    item_name = version_item_name(version)
     item_type_name = version_item_type_name(version.item_type)
     whodunnit_name = version_whodunnit_name(version.whodunnit)
     event_past_tense = version_event_past_tense(version.event)
@@ -18,10 +18,12 @@ module VersionsHelper
     User.where(id: version_whodunnit).first&.name || '[UNKNOWN]'
   end
   
-  def version_item_name(version_item)
-    return '[DELETED]' if version_item.nil?
-    return version_item.name if version_item.respond_to?(:name) 
+  def version_item_name(version)
+    return '[DELETED]' if version.item.nil?
+    return version.item.name if version.item.respond_to?(:name) 
     '[UNKNOWN]'
+  rescue NameError => e
+    "[UNEXPECTED] #{e.missing_name}"
   end
   
   def version_item_type_name(version_item_type)

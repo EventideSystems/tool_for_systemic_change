7335b012f8a0eb0ed190d8a5c9106682faaa3165
Tom Tuddenham <ferrisoxide@gmail.com>
2021-08-25 21:12:34 +0930

    559 add status to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_092815) do
+ActiveRecord::Schema.define(version: 2021_08_24_084240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 2021_08_15_092815) do
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "actual"
     t.index ["checklist_item_id"], name: "index_checklist_item_comments_on_checklist_item_id"
   end
 

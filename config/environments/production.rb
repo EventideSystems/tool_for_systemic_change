1b5534f3b3815291a9b5794748e31f0adce80090
Tom Tuddenham <ferrisoxide@gmail.com>
2021-10-07 04:44:12 +1030

    698 add mailjet to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "wicked_software_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :mailjet
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

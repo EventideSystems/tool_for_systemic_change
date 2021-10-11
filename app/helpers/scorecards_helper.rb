c92a7440dd243ae581285fe83ecc16b0f44d2458
Tom Tuddenham <ferrisoxide@gmail.com>
2021-10-11 19:04:11 +1030

    559 updates for 632

--- app/helpers/scorecards_helper.rb
+++ app/helpers/scorecards_helper.rb
@@ -1,20 +1,25 @@
+# frozen_string_literal: true
+
 module ScorecardsHelper
+
+  ACTUAL_OR_PLANNED =  %w[actual planned].freeze
+
   def lookup_communities
     controller.current_account.communities.order(:name)
   end
-  
+
   def lookup_organisations
     controller.current_account.organisations.order(:name)
   end
-  
+
   def lookup_subsystem_tags
     controller.current_account.subsystem_tags.order(:name)
   end
-  
+
   def lookup_wicked_problems
     controller.current_account.wicked_problems.order(:name)
   end
-  
+
   def display_selected_date
     return 'Select Date' if @selected_date.blank?
     Date.parse(@selected_date).strftime('%B %-d, %Y')
@@ -25,25 +30,29 @@ module ScorecardsHelper
 
     characteristic_data = result[characteristic.id.to_s]
 
-    if characteristic_data['status'].in? ['actual', 'planned']
+    if characteristic_data['status'].in?(ACTUAL_OR_PLANNED) && characteristic_data['comment'].present?
       classes << [
         "#{characteristic_data['status']}#{@focus_areas.index(characteristic.focus_area)+1}",
         characteristic_data['status']
       ]
+      
+      classes << 'hidden' unless characteristic_data['status'] == 'actual'
     else
       if characteristic_data['checked']
         classes << [
           "checked#{@focus_areas.index(characteristic.focus_area)+1}",
           "checked"
         ]
+      end
+
+      if characteristic_data['checked'] && characteristic_data['comment'].blank?
+        classes << 'no-comment'
       else
         classes << 'gap'
       end
 
-      classes << 'no-comment' if characteristic_data['comment'].blank?
+      classes << 'hidden'
     end
-
-    classes << 'hidden' unless characteristic_data['status'] == 'actual'
 
     classes.join(' ')
   end
--- app/javascript/controllers/transition_cards_controller.js
+++ app/javascript/controllers/transition_cards_controller.js
@@ -3,23 +3,35 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "filter", "grid" ]
 
-  selectFilter(event) {
+  connect() {
+    var context = this
+
+    $(this.filterTarget).select2({
+      dropdownAutoWidth: true,
+      multiple: true
+    }).on('select2:select select2:unselect', function (e) {
+      var selected = $(e.target).select2('data').map(function(v) { return v['text']})
+      context.applyFilter(selected)
+    });
+  }
+
+  applyFilter(selected) {
     $(this.gridTarget).find('.cell').addClass("hidden")
     
-    switch(event.target.value)
-    {
-      case "Actual":
-        $(this.gridTarget).find('.cell.actual').removeClass("hidden")
-        break;
-      case "Planned":
-        $(this.gridTarget).find('.cell.planned').removeClass("hidden")
-        break;
-      case "No Comment":
-        $(this.gridTarget).find('.cell.no-comment').removeClass("hidden")
-        break;
-      case "Gap in Effort":
-        $(this.gridTarget).find('.cell.gap').removeClass("hidden")
-        break;
+    if (selected.indexOf('Actual') !== -1) {
+      $(this.gridTarget).find('.cell.actual').removeClass("hidden")
+    }
+
+    if (selected.indexOf('Planned') !== -1) {
+      $(this.gridTarget).find('.cell.planned').removeClass("hidden")
+    }
+
+    if (selected.indexOf('No Comment') !== -1) {
+      $(this.gridTarget).find('.cell.no-comment').removeClass("hidden")
+    }
+
+    if (selected.indexOf('Gap in Effort') !== -1) {
+      $(this.gridTarget).find('.cell.gap').removeClass("hidden")
     }
   }
 }

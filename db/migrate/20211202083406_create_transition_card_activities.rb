e9f3366b88110affffba9f3e660830e9828af49d
Tom Tuddenham <ferrisoxide@gmail.com>
2021-12-08 20:05:43 +1030

    OOB remove unused view from migrations

--- db/migrate/20211202083406_create_transition_card_activities.rb
+++ db/migrate/20211202083406_create_transition_card_activities.rb
@@ -1,24 +1,24 @@
 class CreateTransitionCardActivities < ActiveRecord::Migration[6.1]
   def up
-    connection.execute(<<~SQL)
-      CREATE OR REPLACE VIEW transition_card_activities AS        
-        select
-          scorecards.id as transition_card_id,
-          scorecards.name as transition_card_name,
-          initiatives.id as initiative_id,
-          initiatives.name as initiative_name,
-          characteristics.name as characteristic_name,
-          checklist_item_activities.event,
-          checklist_item_activities.comment,
-          checklist_item_activities.occurred_at,
-          checklist_item_activities.from_status,
-          checklist_item_activities.to_status 
-        from checklist_item_activities
-        inner join checklist_items on checklist_items.id = checklist_item_activities.checklist_item_id
-        inner join characteristics on characteristics.id = checklist_items.characteristic_id
-        inner join initiatives on initiatives.id = checklist_items.initiative_id
-        inner join scorecards on scorecards.id = initiatives.scorecard_id
-    SQL
+    # connection.execute(<<~SQL)
+    #   CREATE OR REPLACE VIEW transition_card_activities AS        
+    #     select
+    #       scorecards.id as transition_card_id,
+    #       scorecards.name as transition_card_name,
+    #       initiatives.id as initiative_id,
+    #       initiatives.name as initiative_name,
+    #       characteristics.name as characteristic_name,
+    #       checklist_item_activities.event,
+    #       checklist_item_activities.comment,
+    #       checklist_item_activities.occurred_at,
+    #       checklist_item_activities.from_status,
+    #       checklist_item_activities.to_status 
+    #     from checklist_item_activities
+    #     inner join checklist_items on checklist_items.id = checklist_item_activities.checklist_item_id
+    #     inner join characteristics on characteristics.id = checklist_items.characteristic_id
+    #     inner join initiatives on initiatives.id = checklist_items.initiative_id
+    #     inner join scorecards on scorecards.id = initiatives.scorecard_id
+    # SQL
   end
 
   def down

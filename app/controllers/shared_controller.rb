48b57deee3da4d526798ab0839c8de13f633021c
Tom Tuddenham <ferrisoxide@gmail.com>
2021-12-11 13:10:13 +1030

    655 display message if scorecard no longer present

--- app/controllers/shared_controller.rb
+++ app/controllers/shared_controller.rb
@@ -7,17 +7,25 @@ class SharedController < ApplicationController
 
   def show
     response.headers.delete 'X-Frame-Options'
+    
+    load_scorecard_and_results
 
-    @focus_areas = FocusArea.ordered_by_group_position
-    @scorecard = Scorecard.find_by_shared_link_id(params[:id])
-
-    @results = TransitionCardSummary.execute(@scorecard, nil, [])
-    @characteristics = Characteristic.includes(focus_area: :focus_area_group).order('focus_areas.position, characteristics.position')
-
+    if @scorecard.present?
+      @focus_areas = FocusArea.ordered_by_group_position
+      @characteristics = Characteristic.includes(focus_area: :focus_area_group).order('focus_areas.position, characteristics.position')
+    end
+    
     if params[:iframe] == 'true'
       render 'show_iframe', layout: false
     else
       render layout: 'embedded'
     end
+  end
+
+  private
+
+  def load_scorecard_and_results
+    @scorecard = Scorecard.find_by_shared_link_id(params[:id])
+    @results = TransitionCardSummary.execute(@scorecard, nil, []) if @scorecard.present?
   end
 end

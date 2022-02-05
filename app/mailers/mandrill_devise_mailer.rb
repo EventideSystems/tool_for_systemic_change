aec86db1459bd379286e9f1b5958aef49b4367d7
Tom Tuddenham <ferrisoxide@gmail.com>
2022-02-05 14:06:15 +1030

    668 added Mandrill support

---
+++ app/mailers/mandrill_devise_mailer.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'mandrill_mailer'
+
+class MandrillDeviseMailer < Devise::Mailer
+  helper :application
+  include Devise::Controllers::UrlHelpers
+  default from: 'hello@wickedlab.com.au'
+  default reply_to: 'hello@wickedlab.com.au'
+
+  def mail(headers = {}, &block)
+    MandrillMailer::TemplateMailer.mandrill_mail(
+      template: 'devise',
+      subject: headers[:subject],
+      to: headers[:to],
+      from: headers[:from],
+      from_name: 'WickedLab',
+      view_content_link: true,
+      vars: {
+        content: render(layout: 'devise_mailer').html_safe
+      },
+      images: [
+        {
+          name: 'logo.png',
+          filename: 'logo.png',
+          content: File.read(Rails.root.join('app/assets/images/logo-long-white-bg.png')),
+          type: 'image/png'
+        }
+      ],
+      important: true,
+      inline_css: true,
+      preserve_recipients: true,
+      merge_language: 'handlebars'
+    ).deliver
+  end
+end

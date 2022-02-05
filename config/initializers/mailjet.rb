babdb31b9d2e4aa40d2b49b1129e401a452d58a3
Tom Tuddenham <ferrisoxide@gmail.com>
2022-02-05 14:06:15 +1030

    668 added Mandrill support

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,5 +1,5 @@
-Mailjet.configure do |config|
-  config.api_key = ENV['MAILJET_PUBLIC_KEY']
-  config.secret_key = ENV['MAILJET_SECRET_KEY']
-  # config.default_from = 'my_registered_mailjet_email@domain.com'
-end
+# Mailjet.configure do |config|
+#   config.api_key = ENV['MAILJET_PUBLIC_KEY']
+#   config.secret_key = ENV['MAILJET_SECRET_KEY']
+#   # config.default_from = 'my_registered_mailjet_email@domain.com'
+# end

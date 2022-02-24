fc04791afdda67f32bdb565c8b5a7c9ea18f75f3
Tom Tuddenham <ferrisoxide@gmail.com>
2022-02-24 21:31:54 +1030

    658 improve initiative checklist item render

--- app/helpers/checklist_items_helper.rb
+++ app/helpers/checklist_items_helper.rb
@@ -1,12 +1,14 @@
-module ChecklistItemsHelper
+# frozen_string_literal: true
 
+module ChecklistItemsHelper
   def current_comment_status_style(checklist_item)
     return '' if checklist_item.current_comment.blank?
+
     checklist_item.current_checklist_item_comment.status.to_s
   end
 
   def comment_statuses_collection
-    ChecklistItemComment.statuses.keys.map do |key| 
+    @comment_statuses_collection ||= ChecklistItemComment.statuses.keys.map do |key|
       [I18n.t("activerecord.attributes.checklist_item_comment/status.#{key}"), key]
     end
   end

161e27032b3df79eaf6c4e8f2bd94525e9409f0d
Tom Tuddenham <ferrisoxide@gmail.com>
2022-03-05 19:27:52 +1030

    658 grid updates

--- db/schema.rb
+++ db/schema.rb
@@ -191,6 +191,7 @@ ActiveRecord::Schema.define(version: 2022_03_08_112711) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "icon_name", default: ""
+    t.string "base_color"
     t.index ["deleted_at"], name: "index_focus_areas_on_deleted_at"
     t.index ["focus_area_group_id"], name: "index_focus_areas_on_focus_area_group_id"
     t.index ["position"], name: "index_focus_areas_on_position"

9882a50ef33227289acd9a2425226ff1c038f883
Tom Tuddenham <ferrisoxide@gmail.com>
2022-03-10 20:58:29 +1030

    658 prior to updating d3

--- app/javascript/controllers/scorecards_controller.js
+++ app/javascript/controllers/scorecards_controller.js
@@ -3,13 +3,28 @@ import Rails from "@rails/ujs";
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "activities" ]
+  static targets = [ "activities", "targetsNetworkMap" ]
 
   loadActivities(event) {
     event.preventDefault()
 
     let activitiesTarget = this.activitiesTarget;
     let loadPath = window.location.pathname + '/activities';
+
+    Rails.ajax({
+      type: "get",
+      url: loadPath,
+      success: function(data) { activitiesTarget.innerHTML = data.body.getInnerHTML() },
+      error: function(data) { alert('Error') }
+    })
+  }
+
+  loadTargetsNetworkMap(event) {
+    event.preventDefault()
+
+    let loadPath = window.location.pathname + '/targets_network_map.json';
+
+    debugger
 
     Rails.ajax({
       type: "get",

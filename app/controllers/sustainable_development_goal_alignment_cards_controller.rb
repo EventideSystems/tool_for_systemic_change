ee2d174600f2c91768ef784c065f2aca5242fbea
Tom Tuddenham <ferrisoxide@gmail.com>
2022-03-12 22:32:57 +1030

    658 first cut for 674, Thematic SDGs & Targets Network Map

--- app/controllers/sustainable_development_goal_alignment_cards_controller.rb
+++ app/controllers/sustainable_development_goal_alignment_cards_controller.rb
@@ -15,11 +15,26 @@ class SustainableDevelopmentGoalAlignmentCardsController < ScorecardsController
         data = EcosystemMaps::TargetsNetwork.new(@scorecard)
         render json: { data: { nodes: data.nodes, links: data.links } }
       end
-
-      format.html do
-
-      end
     end
+  end
+
+  def characteristic
+    @scorecard = current_account.scorecards.find(params[:sustainable_development_goal_alignment_card_id])
+    authorize @scorecard
+
+    @characteristic = Characteristic.find(params[:id])
+
+    checklist_items = \
+      ChecklistItem
+        .where(characteristic: @characteristic, initiative: @scorecard.initiatives)
+        .includes(:checklist_item_comments)
+        .select { |item| item.current_comment_status == 'actual' }
+
+    @initiatives = checklist_items.map(&:initiative).sort_by(&:name)
+
+    @targets = TargetsNetworkMapping.where(characteristic: @characteristic).map(&:focus_area).uniq.sort_by(&:name)
+
+    render 'sustainable_development_goal_alignment_cards/show_tabs/characteristics/show', layout: false
   end
 
   def scorecard_class_name

25b7017406765ebca5247e96077b2d5a3fd37f6b
Tom Tuddenham <ferrisoxide@gmail.com>
2022-03-19 12:34:07 +1030

    704 linked initiatives

--- app/helpers/initiatives_helper.rb
+++ app/helpers/initiatives_helper.rb
@@ -1,21 +1,22 @@
-module InitiativesHelper
+# frozen_string_literal: true
 
+module InitiativesHelper
   def scorecard_label(initiative)
     if initiative.new_record? || initiative.scorecard.nil?
       current_account.scorecard_types.map { |type| type.model_name.human }.join(' / ')
     else
       safe_join([
-        content_tag(:span, "#{initiative.scorecard.model_name.human}", style: 'margin-right: 10px'),
-        link_to(
-          content_tag(
-            :i,
-            '',
-            class: "fa fa-external-link"
-          ),
-          transition_card_path(initiative.scorecard),
-          id: 'initiative-detail-scorecard-link'
-        )
-      ])
+                  content_tag(:span, initiative.scorecard.model_name.human.to_s, style: 'margin-right: 10px'),
+                  link_to(
+                    content_tag(
+                      :i,
+                      '',
+                      class: 'fa fa-external-link'
+                    ),
+                    transition_card_path(initiative.scorecard),
+                    id: 'initiative-detail-scorecard-link'
+                  )
+                ])
     end
   end
 
@@ -30,7 +31,19 @@ module InitiativesHelper
         video_tutorial_title: video_tutorial.name
       }
     ) do
-      safe_join([content_tag(:i, '', class: "fa fa-youtube-play")])
+      safe_join([content_tag(:i, '', class: 'fa fa-youtube-play')])
+    end
+  end
+
+  def linked_initiative_warning(initiative)
+    return '' unless initiative.new_record?
+    return '' unless initiative.scorecard.linked?
+    return '' if initiative.linked_initiative.present?
+
+    content_tag(:div, class: 'form-group row alert alert-warning') do
+      content_tag(:strong, 'Note: ', class: 'mr-1') +
+        "This initiative belongs to a linked #{initiative.scorecard.model_name.human}. " \
+        'Saving this initiative will automaically create a linked initiative in the other card.'
     end
   end
 end

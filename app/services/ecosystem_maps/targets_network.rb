557388fd0940a4cafeb382585b8a01b599ea49d2
Tom Tuddenham <ferrisoxide@gmail.com>
2022-04-06 23:51:04 +0930

    707 WIP, but mostly working

--- app/services/ecosystem_maps/targets_network.rb
+++ app/services/ecosystem_maps/targets_network.rb
@@ -28,13 +28,12 @@ module EcosystemMaps
     def focus_area_nodes
       FocusArea.where(id: TargetsNetworkMapping.pluck(:focus_area_id).uniq).map do |node|
 
-        binding.pry
         mappings = organisations_and_intitiatives.select do |mapping|
           mapping[:focus_area_id] == node.id
         end
 
-        organisation_ids = mappings.map{ |hash| hash[:organisation_ids] }.flatten.uniq
-        initiative_ids = mappings.map{ |hash| hash[:initiative_ids] }.flatten.uniq
+        organisation_ids = mappings.map{ |hash| hash[:organisation_id] }.flatten.compact.uniq.map(&:to_s)
+        initiative_ids = mappings.map{ |hash| hash[:initiative_id] }.flatten.compact.uniq.map(&:to_s)
 
         {
           id: "focus-area-#{node.id}",
@@ -54,8 +53,8 @@ module EcosystemMaps
           mapping[:characteristic_id] == node.id
         end
 
-        organisation_ids = mappings.map{ |hash| hash[:organisation_ids] }.flatten.uniq
-        initiative_ids = mappings.map{ |hash| hash[:initiative_ids] }.flatten.uniq
+        organisation_ids = mappings.map{ |hash| hash[:organisation_id] }.flatten.compact.uniq.map(&:to_s)
+        initiative_ids = mappings.map{ |hash| hash[:initiative_id] }.flatten.compact.uniq.map(&:to_s)
 
         {
           id: "characteristic-#{node.id}",
@@ -69,49 +68,26 @@ module EcosystemMaps
       end
     end
 
-    def organisation_ids
-
 
     def organisations_and_intitiatives
       @organisations_and_intitiatives ||= ActiveRecord::Base.connection.execute(
         <<~SQL
-          with current_checklist_items as (
-            select distinct on (checklist_item_id)
-              events_checklist_item_activities.checklist_item_id as checklist_item_id,
-              characteristics.id as characteristic_id,
-              initiatives.id as initiative_id
-            from events_checklist_item_activities
-            inner join checklist_items on checklist_items.id = events_checklist_item_activities.checklist_item_id
-            inner join characteristics on characteristics.id = checklist_items.characteristic_id
-            inner join initiatives on initiatives.id = checklist_items.initiative_id
-            where events_checklist_item_activities.to_status = 'actual'
-            and initiatives.scorecard_id = #{transition_card.id}
-          )
-          select
-            targets_network_mappings.*,
-            array_agg(initiatives.id) as initiative_ids,
-            array_agg(initiatives_organisations.organisation_id) as organisation_ids
-          from targets_network_mappings
-          inner join current_checklist_items
-            on targets_network_mappings.characteristic_id = current_checklist_items.characteristic_id
-          inner join initiatives
-            on initiatives.id = current_checklist_items.initiative_id
+          select distinct on (checklist_item_id)
+            events_checklist_item_activities.checklist_item_id as checklist_item_id,
+            characteristics.id as characteristic_id,
+            characteristics.focus_area_id as focus_area_id,
+            initiatives.id as initiative_id,
+            initiatives_organisations.organisation_id as organisation_id
+          from events_checklist_item_activities
+          inner join checklist_items on checklist_items.id = events_checklist_item_activities.checklist_item_id
+          inner join characteristics on characteristics.id = checklist_items.characteristic_id
+          inner join initiatives on initiatives.id = checklist_items.initiative_id
           inner join initiatives_organisations
-            on initiatives_organisations.initiative_id = initiatives.id
-          group by
-            targets_network_mappings.id,
-            targets_network_mappings.focus_area_id,
-            targets_network_mappings.characteristic_id
+          on initiatives_organisations.initiative_id = initiatives.id
+          where events_checklist_item_activities.to_status = 'actual'
+          and initiatives.scorecard_id = #{transition_card.id}
         SQL
-      ).map do |row|
-        {
-          id: row['id'],
-          focus_area_id: row['focus_area_id'],
-          characteristic_id: row['characteristic_id'],
-          initiative_ids: row['initiative_ids'].gsub(/{|}/, '').split(',').uniq,
-          organisation_ids: row['organisation_ids'].gsub(/{|}/, '').split(',').uniq
-        }
-      end
+      ).map { |row| row.symbolize_keys }
     end
   end
 end

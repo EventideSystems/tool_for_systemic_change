c13a868e5ed138ce8f263eba256c1ab51f865960
Tom Tuddenham <ferrisoxide@gmail.com>
2022-04-09 15:36:19 +0930

    707 fixed issue with only showing one org

--- app/services/ecosystem_maps/targets_network.rb
+++ app/services/ecosystem_maps/targets_network.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 module EcosystemMaps
-
   class TargetsNetwork
-
     attr_reader :transition_card, :targets_network_mappings
 
     def initialize(transition_card)
@@ -18,7 +18,7 @@ module EcosystemMaps
       end
     end
 
-    # TODO Add orgs to the nodes via the transition_card
+    # TODO: Add orgs to the nodes via the transition_card
     def nodes
       focus_area_nodes + characteristic_nodes
     end
@@ -27,13 +27,12 @@ module EcosystemMaps
 
     def focus_area_nodes
       FocusArea.where(id: TargetsNetworkMapping.pluck(:focus_area_id).uniq).map do |node|
-
         mappings = organisations_and_intitiatives.select do |mapping|
           mapping[:focus_area_id] == node.id
         end
 
-        organisation_ids = mappings.map{ |hash| hash[:organisation_id] }.flatten.compact.uniq.map(&:to_s)
-        initiative_ids = mappings.map{ |hash| hash[:initiative_id] }.flatten.compact.uniq.map(&:to_s)
+        organisation_ids = mappings.map { |hash| hash[:organisation_id] }.flatten.compact.uniq.map(&:to_s)
+        initiative_ids = mappings.map { |hash| hash[:initiative_id] }.flatten.compact.uniq.map(&:to_s)
 
         {
           id: "focus-area-#{node.id}",
@@ -41,20 +40,19 @@ module EcosystemMaps
           color: node.actual_color,
           organisation_ids: organisation_ids,
           initiative_ids: initiative_ids,
-          size: 10,
+          size: 10
         }
       end
     end
 
     def characteristic_nodes
       Characteristic.where(id: TargetsNetworkMapping.pluck(:characteristic_id).uniq).map do |node|
-
         mappings = organisations_and_intitiatives.select do |mapping|
           mapping[:characteristic_id] == node.id
         end
 
-        organisation_ids = mappings.map{ |hash| hash[:organisation_id] }.flatten.compact.uniq.map(&:to_s)
-        initiative_ids = mappings.map{ |hash| hash[:initiative_id] }.flatten.compact.uniq.map(&:to_s)
+        organisation_ids = mappings.map { |hash| hash[:organisation_id] }.flatten.compact.uniq.map(&:to_s)
+        initiative_ids = mappings.map { |hash| hash[:initiative_id] }.flatten.compact.uniq.map(&:to_s)
 
         {
           id: "characteristic-#{node.id}",
@@ -68,7 +66,6 @@ module EcosystemMaps
       end
     end
 
-
     def organisations_and_intitiatives
       @organisations_and_intitiatives ||= ActiveRecord::Base.connection.execute(
         <<~SQL
@@ -76,8 +73,8 @@ module EcosystemMaps
             events_checklist_item_activities.checklist_item_id as checklist_item_id,
             characteristics.id as characteristic_id,
             characteristics.focus_area_id as focus_area_id,
-            initiatives.id as initiative_id,
-            initiatives_organisations.organisation_id as organisation_id
+            array_agg(initiatives.id) as initiative_id,
+            array_agg(initiatives_organisations.organisation_id) as organisation_id
           from events_checklist_item_activities
           inner join checklist_items on checklist_items.id = events_checklist_item_activities.checklist_item_id
           inner join characteristics on characteristics.id = checklist_items.characteristic_id
@@ -86,8 +83,11 @@ module EcosystemMaps
           on initiatives_organisations.initiative_id = initiatives.id
           where events_checklist_item_activities.to_status = 'actual'
           and initiatives.scorecard_id = #{transition_card.id}
+          group by checklist_item_id, characteristics.id
         SQL
-      ).map { |row| row.symbolize_keys }
+      ).map do |row|
+        row.symbolize_keys.transform_values { |v| v.is_a?(String) ? v.gsub(/{|}/, '').split(',') : v }
+      end
     end
   end
 end

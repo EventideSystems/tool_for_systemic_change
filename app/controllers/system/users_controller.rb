ceb1511a1603b7bdf1eb4e2e17b60c6cb48c2351
Tom Tuddenham <ferrisoxide@gmail.com>
2022-06-14 21:30:47 +0930

    760 move export to system users view

--- app/controllers/system/users_controller.rb
+++ app/controllers/system/users_controller.rb
@@ -1,18 +1,27 @@
+# frozen_string_literal: true
+
 module System
   class UsersController < ApplicationController
-
     include SharedUserActions
 
     before_action :authenticate_user!
-    before_action :set_user, only: [:show, :edit, :update, :destroy, :undelete, :resend_invitation]
+    before_action :set_user, only: %i[show edit update destroy undelete resend_invitation]
 
     skip_after_action :verify_policy_scoped
 
-    add_breadcrumb "System"
-    add_breadcrumb "Users", :system_users_path
+    add_breadcrumb 'System'
+    add_breadcrumb 'Users', :system_users_path
 
     def index
-      @users = UserPolicy::SystemScope.new(pundit_user, User.all).resolve.page params[:page]
+      respond_to do |format|
+        format.html do |_html|
+          @users = UserPolicy::SystemScope.new(pundit_user, User.all).resolve.page params[:page]
+        end
+        format.csv do
+          @users = @users = UserPolicy::SystemScope.new(pundit_user, User.all).resolve
+          send_data users_to_csv(@users), type: Mime[:csv], filename: 'users.csv'
+        end
+      end
     end
 
     def show
@@ -23,7 +32,7 @@ module System
     def new
       @user = User.new
       authorize @user
-      add_breadcrumb "New"
+      add_breadcrumb 'New'
     end
 
     def edit
@@ -36,9 +45,10 @@ module System
       account_role = user_params.delete(:account_role)
 
       if current_account.users.where(email: @user.email).exists?
-        redirect_to system_users_path, alert: "A user with the email '#{user.email}' is already a member of this account."
+        redirect_to system_users_path,
+                    alert: "A user with the email '#{user.email}' is already a member of this account."
       elsif max_users_reached?
-        redirect_to users_path, alert: "You have reached the maximum number of users for this account."
+        redirect_to users_path, alert: 'You have reached the maximum number of users for this account.'
       else
         @user.accounts_users.build(account: current_account, role: account_role)
 
@@ -65,7 +75,7 @@ module System
       if current_account_user
         current_account_user.update(account_role: account_role)
       elsif max_users_reached?
-        redirect_to users_path, alert: "You have reached the maximum number of users for this account." and return
+        redirect_to users_path, alert: 'You have reached the maximum number of users for this account.' and return
       else
         @user.accounts_users.build(account: current_account, account_role: account_role)
       end
@@ -95,6 +105,7 @@ module System
 
     def content_subtitle
       return @user.display_name if @user.present?
+
       super
     end
 
@@ -103,6 +114,7 @@ module System
     # SMELL: This is a duplicate of the code in the InvitationsContoller class.
     def max_users_reached?
       return false if current_account.max_users.zero? || current_account.max_users.blank?
+
       current_account.users.count >= current_account.max_users
     end
 
@@ -121,8 +133,42 @@ module System
         :name,
         :email,
         :system_role,
-        accounts_users_attributes: [:id, :account_role, :_destroy]
+        accounts_users_attributes: %i[id account_role _destroy]
       )
+    end
+
+    def users_to_csv(users)
+      account_ids = policy_scope(Account).all.pluck(:id)
+
+      CSV.generate(force_quotes: true) do |csv|
+        csv << [
+          'Name',
+          'Email',
+          'Account name',
+          'Date created',
+          'Account Expiry date',
+          'Systems Role',
+          'Account Role',
+          'Last logged in date'
+        ]
+
+        users.each do |user|
+          user.accounts_users.each do |accounts_user|
+            next unless account_ids.include?(accounts_user.account_id)
+
+            csv << [
+              user.name,
+              user.email,
+              accounts_user.account.name,
+              accounts_user.account.created_at.strftime('%d/%m/%Y'),
+              accounts_user.account.expires_on&.strftime('%d/%m/%Y') || '',
+              user.system_role.titleize,
+              accounts_user.account_role.titleize,
+              user.last_sign_in_at&.strftime('%d/%m/%Y') || ''
+            ]
+          end
+        end
+      end
     end
   end
 end

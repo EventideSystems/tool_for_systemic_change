730f2a8428d2b78b257ebfbb0bb25e3c7f895d49
Tom Tuddenham <ferrisoxide@gmail.com>
2022-06-18 14:38:35 +0930

    763 tweaks to policies

--- app/policies/community_policy.rb
+++ app/policies/community_policy.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class CommunityPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       resolve_to_current_account
     end
   end
-  
+
   def show?
     system_admin? || account_any_role?(record.account)
   end
-  
+
   def create?
     system_admin? || account_admin?(record.account)
   end
@@ -16,7 +18,7 @@ class CommunityPolicy < ApplicationPolicy
   def update?
     system_admin? || account_admin?(record.account)
   end
-  
+
   def destroy?
     system_admin? || account_admin?(record.account)
   end

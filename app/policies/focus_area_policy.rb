f8c62df67dc3f7325eb7e65c23f71f9a2b463aaf
Tom Tuddenham <ferrisoxide@gmail.com>
2022-06-21 18:03:52 +0930

    Revert "763 tweaks to policies"
    This reverts commit 7e57cff218d63d32dcc5dfd924d188b43cbfb36b.

--- app/policies/focus_area_policy.rb
+++ app/policies/focus_area_policy.rb
@@ -1,28 +1,26 @@
-# frozen_string_literal: true
-
 class FocusAreaPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope
     end
   end
-
+  
   def index?
     system_admin?
   end
-
+  
   def show?
     system_admin?
   end
-
+  
   def create?
     system_admin?
   end
-
+  
   def update?
     system_admin?
   end
-
+  
   def destroy?
     system_admin?
   end

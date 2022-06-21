79c4a67bcd993402e6ace84aae8847a4f30fdb4d
Tom Tuddenham <ferrisoxide@gmail.com>
2022-06-21 18:49:08 +0930

    Revert "763 tweaks to policies"
    This reverts commit bdfd293d9a6e583c8219844fd00394e8d744d5e1.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -97,5 +97,4 @@ class ApplicationController < ActionController::Base
   # flash[:error] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
    redirect_to(request.referrer || root_path)
   end
-
 end

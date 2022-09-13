d2dd106c0138036a3ad23279b882efad28d432ba
Tom Tuddenham <ferrisoxide@gmail.com>
2022-09-14 07:31:58 +0930

    809 add subsytem tag filter

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -3,9 +3,17 @@
 class ScorecardsController < ApplicationController
   before_action :set_scorecard,
                 only: %i[
-                  show edit update destroy
-                  show_shared_link copy copy_options merge merge_options
-                  activities changes
+                  show
+                  edit
+                  update
+                  destroy
+                  show_shared_link
+                  copy
+                  copy_options
+                  merge
+                  merge_options
+                  activities
+                  changes
                 ]
 
   before_action :set_active_tab, only: [:show]
@@ -19,12 +27,12 @@ class ScorecardsController < ApplicationController
     @selected_date = params[:selected_date]
     @parsed_selected_date = @selected_date.blank? ? nil : Date.parse(@selected_date)
 
-    @selected_tags = if params[:selected_tags].blank?
-                       []
-                     else
-                       SubsystemTag.where(account: current_account,
-                                          name: params[:selected_tags])
-                     end
+    @selected_tags =
+      if params[:selected_tags].blank?
+        []
+      else
+        SubsystemTag.where(account: current_account, name: params[:selected_tags])
+      end
 
     @focus_areas = FocusArea
                    .includes(:characteristics)
@@ -42,60 +50,62 @@ class ScorecardsController < ApplicationController
 
     @linked_initiatives = build_linked_intiatives(source_scorecard, target_scorecard)
 
-    #@results = ScorecardGrid.execute(@scorecard, @parsed_selected_date, @selected_tags)
+    @results = NewScorecardGrid.execute(@scorecard, @parsed_selected_date, @selected_tags)
 
-    @results = NewScorecardGrid.execute(@scorecard, @parsed_selected_date)
-
-    add_breadcrumb @scorecard.name
+    add_breadcrumb(@scorecard.name)
 
     respond_to do |format|
       format.html
       format.pdf do
         # TODO: Convert PDF to use transition card summary data
-        @initiatives = if @parsed_selected_date.present?
-                         @scorecard.initiatives
-                                   .where('started_at <= ? OR started_at IS NULL', @parsed_selected_date)
-                                   .where('finished_at >= ? OR finished_at IS NULL', @parsed_selected_date)
-                                   .order(name: :asc)
-                       else
-                         @scorecard.initiatives.order(name: :asc)
-                       end
+        @initiatives =
+          if @parsed_selected_date.present?
+            @scorecard.initiatives
+                      .where('started_at <= ? OR started_at IS NULL', @parsed_selected_date)
+                      .where('finished_at >= ? OR finished_at IS NULL', @parsed_selected_date)
+                      .order(name: :asc)
+          else
+            @scorecard.initiatives.order(name: :asc)
+          end
 
-        @initiatives = if @selected_tags.present?
-                         tag_ids = @selected_tags.map(&:id)
-                         @initiatives
-                           .distinct
-                           .joins(:initiatives_subsystem_tags)
-                           .where('initiatives_subsystem_tags.subsystem_tag_id' => tag_ids)
-                           .select('initiatives.*, lower(initiatives.name)')
-                       else
-                         @initiatives
-                       end
+        @initiatives =
+          if @selected_tags.present?
+            tag_ids = @selected_tags.map(&:id)
+            @initiatives
+              .distinct
+              .joins(:initiatives_subsystem_tags)
+              .where('initiatives_subsystem_tags.subsystem_tag_id': tag_ids)
+              .select('initiatives.*, lower(initiatives.name)')
+          else
+            @initiatives
+          end
 
         pdf = ScorecardPdfGenerator.new(
           scorecard: @scorecard,
           initiatives: @initiatives,
           focus_areas: @focus_areas
         ).perform
-        send_data pdf.render,
-                  filename: "transition_card_#{@scorecard.id}.pdf",
-                  type: 'application/pdf',
-                  disposition: 'inline'
+        send_data(
+          pdf.render,
+          filename: "transition_card_#{@scorecard.id}.pdf",
+          type: 'application/pdf',
+          disposition: 'inline'
+        )
       end
     end
   end
 
   def new
     @scorecard = current_account.scorecards.build(type: scorecard_class_name)
-    authorize @scorecard, policy_class: ScorecardPolicy
+    authorize(@scorecard, policy_class: ScorecardPolicy)
 
     @scorecard.initiatives.build
 
-    add_breadcrumb 'New'
+    add_breadcrumb('New')
   end
 
   def edit
-    add_breadcrumb @scorecard.name
+    add_breadcrumb(@scorecard.name)
 
     source_scorecard = @scorecard
     target_scorecard = @scorecard.linked_scorecard
@@ -105,16 +115,16 @@ class ScorecardsController < ApplicationController
 
   def create
     @scorecard = current_account.scorecards.build(scorecard_params)
-    authorize @scorecard, policy_class: ScorecardPolicy
+    authorize(@scorecard, policy_class: ScorecardPolicy)
 
     respond_to do |format|
       if @scorecard.save
         SynchronizeLinkedScorecard.call(@scorecard, linked_initiatives_params)
-        format.html { redirect_to @scorecard, notice: "#{@scorecard.model_name.human} was successfully created." }
-        format.json { render :show, status: :created, location: @scorecard }
+        format.html { redirect_to(@scorecard, notice: "#{@scorecard.model_name.human} was successfully created.") }
+        format.json { render(:show, status: :created, location: @scorecard) }
       else
-        format.html { render :new }
-        format.json { render json: @scorecard.errors, status: :unprocessable_entity }
+        format.html { render(:new) }
+        format.json { render(json: @scorecard.errors, status: :unprocessable_entity) }
       end
     end
   end
@@ -129,11 +139,11 @@ class ScorecardsController < ApplicationController
 
         SynchronizeLinkedScorecard.call(@scorecard, linked_initiatives_params)
 
-        format.html { redirect_to @scorecard, notice: "#{@scorecard.model_name.human} was successfully updated." }
-        format.json { render :show, status: :ok, location: @scorecard }
+        format.html { redirect_to(@scorecard, notice: "#{@scorecard.model_name.human} was successfully updated.") }
+        format.json { render(:show, status: :ok, location: @scorecard) }
       else
-        format.html { render :edit }
-        format.json { render json: @scorecard.errors, status: :unprocessable_entity }
+        format.html { render(:edit) }
+        format.json { render(json: @scorecard.errors, status: :unprocessable_entity) }
       end
     end
   end
@@ -143,7 +153,7 @@ class ScorecardsController < ApplicationController
 
     initiative_ids = @scorecard.initiatives.pluck(:id)
 
-    # SMELL: Move all this to an event object
+    # SMELL: Move all this to an event object - or better, setup up destroy dependencies / callbacks
     ChecklistItem.where(initiative_id: initiative_ids).delete_all
     ChecklistItem.where(initiative_id: initiative_ids).delete_all
     InitiativesOrganisation.where(initiative_id: initiative_ids).delete_all
@@ -157,25 +167,25 @@ class ScorecardsController < ApplicationController
       format.html do
         case @scorecard.class.name
         when 'TransitionCard'
-          redirect_to transition_cards_path, notice: notice
+          redirect_to(transition_cards_path, notice: notice)
         when 'SustainableDevelopmentGoalAlignmentCard'
-          redirect_to sustainable_development_goal_alignment_cards_path, notice: notice
+          redirect_to(sustainable_development_goal_alignment_cards_path, notice: notice)
         else
-          raise "Unknown scorecard type: #{klass.name}"
+          raise("Unknown scorecard type: #{klass.name}")
         end
       end
-      format.json { head :no_content }
+      format.json { head(:no_content) }
     end
   end
 
   def shared; end
 
   def show_shared_link
-    render layout: false
+    render(layout: false)
   end
 
   def copy_options
-    render layout: false
+    render(layout: false)
   end
 
   def copy
@@ -186,26 +196,21 @@ class ScorecardsController < ApplicationController
     respond_to do |format|
       if @copied_scorecard.present?
         format.html do
-          redirect_to @copied_scorecard,
-                      notice: "#{@copied_scorecard.model_name.human} was successfully copied."
+          redirect_to(@copied_scorecard, notice: "#{@copied_scorecard.model_name.human} was successfully copied.")
         end
-        format.json { render :show, status: :ok, location: @copied_scorecard }
+        format.json { render(:show, status: :ok, location: @copied_scorecard) }
       else
-        format.html { render :edit }
-        format.json { render json: @copied_scorecard.errors, status: :unprocessable_entity }
+        format.html { render(:edit) }
+        format.json { render(json: @copied_scorecard.errors, status: :unprocessable_entity) }
       end
     end
   end
 
   def merge_options
     @other_scorecards = \
-      current_account
-      .scorecards
-      .where(type: @scorecard.type)
-      .where.not(id: @scorecard.id)
-      .order('lower(name)')
+      current_account.scorecards.where(type: @scorecard.type).where.not(id: @scorecard.id).order('lower(name)')
 
-    render layout: false
+    render(layout: false)
   end
 
   def merge
@@ -214,44 +219,45 @@ class ScorecardsController < ApplicationController
     respond_to do |format|
       if @scorecard.present?
         format.html do
-          card_path = case @scorecard.type
-                      when 'TransitionCard' then transition_card_path(@merged_scorecard)
-                      when 'SustainableDevelopmentGoalAlignmentCard' then sustainable_development_goal_alignment_card_path(@merged_scorecard)
-                      end
+          card_path =
+            case @scorecard.type
+            when 'TransitionCard' then transition_card_path(@merged_scorecard)
+            when 'SustainableDevelopmentGoalAlignmentCard' then sustainable_development_goal_alignment_card_path(@merged_scorecard)
+            end
 
-          redirect_to card_path, notice: "#{@scorecard.model_name.human} were successfully merged."
+          redirect_to(card_path, notice: "#{@scorecard.model_name.human} were successfully merged.")
         end
-        format.json { render :show, status: :ok, location: @merged_scorecard }
+        format.json { render(:show, status: :ok, location: @merged_scorecard) }
       else
-        format.html { render :edit }
-        format.json { render json: @merged_scorecard.errors, status: :unprocessable_entity }
+        format.html { render(:edit) }
+        format.json { render(json: @merged_scorecard.errors, status: :unprocessable_entity) }
       end
     end
   end
 
   def ecosystem_maps_organisations
-    if params[:id].to_i.to_s == params[:id]
+    if Integer(params[:id], 10).to_s == params[:id]
       @scorecard = current_account.scorecards.find(params[:id])
-      authorize @scorecard
+      authorize(@scorecard)
     else
       @scorecard = Scorecard.find_by(shared_link_id: params[:id])
     end
 
     data = EcosystemMaps::Organisations.new(@scorecard)
 
-    render json: { data: { nodes: data.nodes, links: data.links } }
+    render(json: { data: { nodes: data.nodes, links: data.links } })
   end
 
   def activities
     @activities = Events::TransitionCardActivity.where(transition_card_id: @scorecard.id).order(occurred_at: :desc)
 
-    render partial: '/scorecards/show_tabs/activities', locals: { activities: @activities }
+    render(partial: '/scorecards/show_tabs/activities', locals: { activities: @activities })
   end
 
   def changes
     @changes = @scorecard.scorecard_changes.order(occurred_at: :desc)
 
-    render partial: '/scorecards/show_tabs/changes', locals: { changes: @changes }
+    render(partial: '/scorecards/show_tabs/changes', locals: { changes: @changes })
   end
 
   def content_subtitle
@@ -264,11 +270,13 @@ class ScorecardsController < ApplicationController
     source_scorecard = current_account.scorecards.find(params[:id])
     target_scorecard = current_account.scorecards.find(params[:target_id])
 
-    authorize source_scorecard, policy_class: ScorecardPolicy
-    authorize target_scorecard, policy_class: ScorecardPolicy
+    authorize(source_scorecard, policy_class: ScorecardPolicy)
+    authorize(target_scorecard, policy_class: ScorecardPolicy)
 
-    render partial: '/scorecards/show_tabs/linked_initiatives',
-           locals: { linked_initiatives: build_linked_intiatives(source_scorecard, target_scorecard) }
+    render(
+      partial: '/scorecards/show_tabs/linked_initiatives',
+      locals: { linked_initiatives: build_linked_intiatives(source_scorecard, target_scorecard) }
+    )
   end
 
   private
@@ -276,13 +284,15 @@ class ScorecardsController < ApplicationController
   def build_linked_intiatives(source_scorecard, target_scorecard)
     return [] if target_scorecard.blank?
 
-    source_initiatives = source_scorecard.initiatives.each_with_object({}) do |initiative, hash|
-      hash[initiative.name] = initiative
-    end
+    source_initiatives =
+      source_scorecard.initiatives.each_with_object({}) do |initiative, hash|
+        hash[initiative.name] = initiative
+      end
 
-    target_initiatives = target_scorecard.initiatives.each_with_object({}) do |initiative, hash|
-      hash[initiative.name] = initiative
-    end
+    target_initiatives =
+      target_scorecard.initiatives.each_with_object({}) do |initiative, hash|
+        hash[initiative.name] = initiative
+      end
 
     all_names = (source_initiatives.keys + target_initiatives.keys).uniq.sort
 
@@ -313,7 +323,7 @@ class ScorecardsController < ApplicationController
 
   def set_scorecard
     @scorecard = current_account.scorecards.find(params[:id])
-    authorize @scorecard, policy_class: ScorecardPolicy
+    authorize(@scorecard, policy_class: ScorecardPolicy)
   end
 
   def set_active_tab
@@ -328,10 +338,10 @@ class ScorecardsController < ApplicationController
     case controller_name
     when 'transition_cards'
       if @scorecard.type == 'SustainableDevelopmentGoalAlignmentCard'
-        redirect_to sustainable_development_goal_alignment_card_path(@scorecard)
+        redirect_to(sustainable_development_goal_alignment_card_path(@scorecard))
       end
     when 'sustainable_development_goal_alignment_cards'
-      redirect_to transition_card_path(@scorecard) if @scorecard.type == 'TransitionCard'
+      redirect_to(transition_card_path(@scorecard)) if @scorecard.type == 'TransitionCard'
     end
   end
 
@@ -367,14 +377,16 @@ class ScorecardsController < ApplicationController
         :contact_position,
         :notes,
         :type,
-        { initiatives_organisations_attributes: %i[
+        {
+          initiatives_organisations_attributes: %i[
             _destroy
             organisation_id
           ],
           initiatives_subsystem_tags_attributes: %i[
             _destroy
             subsystem_tag_id
-          ] }
+          ]
+        }
       ]
     ).tap do |params| # NOTE: Dupe of code in initiatives controller
       params[:type] = scorecard_class_name
@@ -400,8 +412,11 @@ class ScorecardsController < ApplicationController
 
       unless params[:initiatives_attributes].blank?
         params[:initiatives_attributes].each do |initiative_key, _|
-          next unless params.dig(:initiatives_attributes, initiative_key,
-                                 :initiatives_subsystem_tags_attributes).present?
+          next unless params.dig(
+            :initiatives_attributes,
+            initiative_key,
+            :initiatives_subsystem_tags_attributes
+          ).present?
 
           params[:initiatives_attributes][initiative_key][:initiatives_subsystem_tags_attributes].reject! do |key, value|
             value[:_destroy] != '1' && (

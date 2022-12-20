195cd0c03e177fb3c78ff79c398a4c1438a52cc4
Tom Tuddenham <ferrisoxide@gmail.com>
2022-12-20 15:41:01 +1030

    663 upgrade ruby, fix toast, fix policies

--- app/javascript/components/toasts.js
+++ app/javascript/components/toasts.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('DOMContentLoaded', function() {
   $('.toast-auto-hide').toast({ autohide: true, delay: 2500 })
   $('.toast-auto-hide').toast('show')
 });
--- app/models/sector.rb
+++ app/models/sector.rb
@@ -7,10 +7,21 @@ class Sector < ApplicationRecord
   belongs_to :account, optional: true
 
   has_many :accounts
+  has_many :organisations
+
+  before_destroy :check_no_longer_used!, prepend: true
 
   scope :system_sectors, -> { where(account_id: nil) }
 
   def system_sector?
     account_id.nil?
+  end
+
+  private
+
+  def check_no_longer_used!
+    return true if organisations.empty?
+    errors.add(:base, "This sector is still in use")
+    throw(:abort)
   end
 end

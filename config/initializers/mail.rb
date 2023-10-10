c6858ce79273eb89f3af4731ad8513acb169d73c
Tom Tuddenham <ferrisoxide@gmail.com>
2023-10-10 21:53:44 +1030

    898 initial setup for mailjet

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.mandrillapp.com',
-  port: 587,
-  user_name: ENV['MANDRILL_USERNAME'],
-  password: ENV['MANDRILL_API_KEY'],
-  domain: 'wickedlab.com.au'
-}
-ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.mandrillapp.com',
+#   port: 587,
+#   user_name: ENV['MANDRILL_USERNAME'],
+#   password: ENV['MANDRILL_API_KEY'],
+#   domain: 'wickedlab.com.au'
+# }
+# ActionMailer::Base.delivery_method = :smtp
 
-MandrillMailer.configure do |config|
-  config.api_key = ENV['MANDRILL_API_KEY']
-  config.deliver_later_queue_name = :default
-end
+# MandrillMailer.configure do |config|
+#   config.api_key = ENV['MANDRILL_API_KEY']
+#   config.deliver_later_queue_name = :default
+# end

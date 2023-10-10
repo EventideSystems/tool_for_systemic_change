4f54e8270092286244d88d24f7e28bb55833d1a0
Tom Tuddenham <ferrisoxide@gmail.com>
2023-10-10 21:53:44 +1030

    898 initial setup for mailjet

--- app/mailers/mandrill_devise_mailer.rb
+++ app/mailers/mandrill_devise_mailer.rb
@@ -2,35 +2,35 @@
 
 require 'mandrill_mailer'
 
-class MandrillDeviseMailer < Devise::Mailer
-  helper :application
-  include Devise::Controllers::UrlHelpers
-  default from: 'hello@wickedlab.com.au'
-  default reply_to: 'hello@wickedlab.com.au'
+# class MandrillDeviseMailer < Devise::Mailer
+#   helper :application
+#   include Devise::Controllers::UrlHelpers
+#   default from: 'hello@wickedlab.com.au'
+#   default reply_to: 'hello@wickedlab.com.au'
 
-  def mail(headers = {}, &block)
-    MandrillMailer::TemplateMailer.mandrill_mail(
-      template: 'devise',
-      subject: headers[:subject],
-      to: headers[:to],
-      from: headers[:from],
-      from_name: 'Wicked Lab',
-      view_content_link: true,
-      vars: {
-        content: render(layout: 'devise_mailer').html_safe
-      },
-      images: [
-        {
-          name: 'logo.png',
-          filename: 'logo.png',
-          content: File.read(Rails.root.join('app/assets/images/logo-long-white-bg-pdf.png')),
-          type: 'image/png'
-        }
-      ],
-      important: true,
-      inline_css: true,
-      preserve_recipients: true,
-      merge_language: 'handlebars'
-    ).deliver
-  end
-end
+#   def mail(headers = {}, &block)
+#     MandrillMailer::TemplateMailer.mandrill_mail(
+#       template: 'devise',
+#       subject: headers[:subject],
+#       to: headers[:to],
+#       from: headers[:from],
+#       from_name: 'Wicked Lab',
+#       view_content_link: true,
+#       vars: {
+#         content: render(layout: 'devise_mailer').html_safe
+#       },
+#       images: [
+#         {
+#           name: 'logo.png',
+#           filename: 'logo.png',
+#           content: File.read(Rails.root.join('app/assets/images/logo-long-white-bg-pdf.png')),
+#           type: 'image/png'
+#         }
+#       ],
+#       important: true,
+#       inline_css: true,
+#       preserve_recipients: true,
+#       merge_language: 'handlebars'
+#     ).deliver
+#   end
+# end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,7 +16,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
-  config.mailer = 'MandrillDeviseMailer'
+  # config.mailer = 'MandrillDeviseMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'

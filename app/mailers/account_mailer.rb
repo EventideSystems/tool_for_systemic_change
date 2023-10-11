507dbfc62eb86619e6d01b54f1311438f6f803a1
Tom Tuddenham <ferrisoxide@gmail.com>
2023-10-11 19:26:10 +1030

    898 fix account expiry mailer

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -2,13 +2,12 @@
 
 class AccountMailer < ApplicationMailer
   def expiry_warning(account, user)
-    user_name = user.name.presence || 'Wicked Lab User'
-    expires_on = account.expires_on&.strftime('%B %d, %Y').presence || 'sometime in the future'
-
-    send_mail(
+    @user_name = user.name.presence || 'Wicked Lab User'
+    @expires_on = account.expires_on&.strftime('%B %d, %Y').presence || 'sometime in the future'
+    @pricing_pdf = 'Pricing.Summary_TC.+.SDG.pdf'
+    mail(
       subject: 'Your account is about to expire',
-      to: user.email,
-      content: render_template('expiry_warning', user_name: user_name, expires_on: expires_on)
+      to: user.email
     )
   end
 end

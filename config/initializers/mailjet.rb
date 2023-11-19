cac460a5a9c734c255fdd83934733fdb21c5a2c6
Tom Tuddenham <ferrisoxide@gmail.com>
2023-11-19 21:45:18 +1030

    914 and 915

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,7 +2,7 @@
 Mailjet.configure do |config|
   config.api_key = Rails.application.credentials.mailjet.api_key
   config.secret_key = Rails.application.credentials.mailjet.secret
-  config.default_from = 'hello@wickedlab.com.au'
+  config.default_from = 'hello@toolforsystemicchange.com'
   # Mailjet API v3.1 is at the moment limited to Send API.
   # We’ve not set the version to it directly since there is no other endpoint in that version.
   # We recommend you create a dedicated instance of the wrapper set with it to send your emails.

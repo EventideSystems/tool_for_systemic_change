5e873bb3f5a0e080b3f81018af0f6388c6f9d3c2
Tom Tuddenham <ferrisoxide@gmail.com>
2023-12-14 09:17:06 +1030

    910 change footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,13 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   skip_after_action :verify_policy_scoped
-  
+  skip_after_action :verify_authorized
+
   layout 'home'
-  
+
   def index
-    redirect_to(dashboard_path) if user_signed_in? 
+    redirect_to(dashboard_path) if user_signed_in?
   end
+
+  def contributors; end
 end

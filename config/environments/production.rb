25f5c3bd3b31d9dc66aa3cd5d7b8ca63beb4e4a7
Tom Tuddenham <ferrisoxide@gmail.com>
2024-06-27 21:37:45 +0930

    add config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

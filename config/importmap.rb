7f8048dca15b1dfb0745d392759c5c0f8e6bd166
Tom Tuddenham <ferrisoxide@gmail.com>
2024-10-06 11:18:30 +1030

    gen-2 removed split-grid for now

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from 'app/javascript/components', under: 'components'
 pin "tailwindcss-animate" # @1.0.7
-pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @3.4.7
 pin "hotkeys-js" # @3.13.7
 pin "split.js" # @1.6.5
+
+
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @3.4.13

a0763308445c4f0d2d75a3df1e37f2c3a8f774a4
Tom Tuddenham <ferrisoxide@gmail.com>
2024-10-23 22:07:01 +1030

    new color model

--- app/helpers/labels_helper.rb
+++ app/helpers/labels_helper.rb
@@ -2,38 +2,6 @@
 
 module LabelsHelper
 
-  # Source: https://www.redguava.com.au/2011/10/lighten-or-darken-a-hexadecimal-color-in-ruby-on-rails/
-
-  # Amount should be a decimal between 0 and 1. Lower means darker
-  def darken_color(hex_color, amount=0.4)
-    hex_color = hex_color.gsub('#','')
-    rgb = hex_color.scan(/../).map {|color| color.hex}
-    rgb[0] = (rgb[0].to_i * amount).round
-    rgb[1] = (rgb[1].to_i * amount).round
-    rgb[2] = (rgb[2].to_i * amount).round
-    "#%02x%02x%02x" % rgb
-  end
-
-  # Amount should be a decimal between 0 and 1. Higher means lighter
-  def lighten_color(hex_color, amount=0.6)
-    hex_color = hex_color.gsub('#','')
-    rgb = hex_color.scan(/../).map {|color| color.hex}
-    rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
-    rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
-    rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
-    "#%02x%02x%02x" % rgb
-  end
-
-  def contrasting_text_color(hex_color)
-    color = hex_color.gsub('#','')
-    convert_to_brightness_value(color) > 382.5 ? darken_color(color) : lighten_color(color)
-  end
-
-  def convert_to_brightness_value(hex_color)
-     (hex_color.scan(/../).map {|color| color.hex}).sum
-  end
-
-
   def hex_to_rgb(hex)
     hex = hex.gsub('#', '')
     r = hex[0..1].to_i(16)

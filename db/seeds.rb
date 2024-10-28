2650fddbff157ccac7a588f629b8c77d99ff4bad
Tom Tuddenham <ferrisoxide@gmail.com>
2024-10-28 22:37:40 +1030

    gen-2 basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# Create a default system admin user
+User.find_or_create_by!(email: 'system_admin@obsekio.org') do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.system_role = 'admin'
+end
+
+# Create a default user
+
+user = User.find_or_create_by!(email: 'user@obsekio.org') do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.system_role = 'member'
+end
+
+# Create an account admin user
+#
+account_admmin_user = User.find_or_create_by!(email: 'admin@obsekio.org') do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.system_role = 'member'
+end
+
+# Create a default account
+
+Account.find_or_create_by!(name: 'Default Account') do |account|
+  account.accounts_users << AccountsUser.new(user: user, account_role: 'member')
+  account.accounts_users << AccountsUser.new(user: account_admmin_user, account_role: 'admin')
+  account.save!
+end

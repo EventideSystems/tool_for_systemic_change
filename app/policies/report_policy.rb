cd3819891dbcc4d02bc4f020375e29e34da236b6
Tom Tuddenham <ferrisoxide@gmail.com>
2025-02-22 18:20:23 +1030

    1124 relaxed access to subsystem tag report

--- app/policies/report_policy.rb
+++ app/policies/report_policy.rb
@@ -25,6 +25,10 @@ class ReportPolicy < ApplicationPolicy # rubocop:disable Style/Documentation
     true
   end
 
+  def subsystem_summary?
+    true
+  end
+
   def cross_account_percent_actual?
     user_has_active_accounts_with_admin_role?
   end
@@ -35,10 +39,6 @@ class ReportPolicy < ApplicationPolicy # rubocop:disable Style/Documentation
 
   def cross_account_percent_actual_by_focus_area_tabbed?
     user_has_active_accounts_with_admin_role?
-  end
-
-  def subsystem_summary?
-    system_admin? || account_admin?(current_account)
   end
 
   private

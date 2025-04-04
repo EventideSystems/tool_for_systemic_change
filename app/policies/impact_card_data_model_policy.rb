9cccf9a36f75e017ca2344e1e5ce2c0e0ce58596
Tom Tuddenham <ferrisoxide@gmail.com>
2025-04-04 16:40:41 +1030

    1106 fixed issue with system admins not seeing current workspace models

--- app/policies/impact_card_data_model_policy.rb
+++ app/policies/impact_card_data_model_policy.rb
@@ -5,7 +5,14 @@ class ImpactCardDataModelPolicy < ApplicationPolicy
   # Scope class for LabelPolicy
   class Scope < Scope
     def resolve
-      scope.where(workspace_id: current_user.workspace_ids).or(ImpactCardDataModel.where(system_model: true))
+      scope.where(workspace_id: workspace_ids).or(ImpactCardDataModel.where(system_model: true))
+    end
+
+    def workspace_ids
+      current_user.workspace_ids.to_a.then do |workspace_ids|
+        workspace_ids.push(current_workspace.id) if system_admin?
+        workspace_ids.uniq
+      end
     end
   end
 

99cacbffc50b438097461c8c8569365bc7a4bd30
Tom Tuddenham <ferrisoxide@gmail.com>
2025-06-12 15:24:17 +0930

    out-of-band skip rest of rename_impact_card_data_model_table

--- db/migrate/20250424031446_rename_impact_card_data_model_table.rb
+++ db/migrate/20250424031446_rename_impact_card_data_model_table.rb
@@ -1,7 +1,7 @@
 class RenameImpactCardDataModelTable < ActiveRecord::Migration[8.0]
   def change
    # rename_table :impact_card_data_models, :data_models
-    rename_column :scorecards, :impact_card_data_model_id, :data_model_id
-    rename_column :focus_area_groups, :impact_card_data_model_id, :data_model_id
+   # rename_column :scorecards, :impact_card_data_model_id, :data_model_id
+   # rename_column :focus_area_groups, :impact_card_data_model_id, :data_model_id
   end
 end

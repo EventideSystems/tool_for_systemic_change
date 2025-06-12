25698df086013ec048ae0e054352f6af331cb0be
Tom Tuddenham <ferrisoxide@gmail.com>
2025-06-12 15:13:47 +0930

    out-of-band skip update_scorecard_type_characteristics_to_version_7

--- db/migrate/20250424032115_update_scorecard_type_characteristics_to_version_8.rb
+++ db/migrate/20250424032115_update_scorecard_type_characteristics_to_version_8.rb
@@ -1,5 +1,9 @@
 class UpdateScorecardTypeCharacteristicsToVersion8 < ActiveRecord::Migration[8.0]
-  def change
-    update_view :scorecard_type_characteristics, version: 8, revert_to_version: 7
+  def up
+    update_view :scorecard_type_characteristics, version: 8
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, "Cannot revert to previous version of scorecard_type_characteristics view"
   end
 end
